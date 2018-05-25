e669c1aada1b27695cddd8808646df9c00a06475
polygraphene <polygraphene@gmail.com>
2018-05-26 01:45:13 +0900

    Fix release.bat

--- release.bat
+++ release.bat
@@ -2,12 +2,4 @@ mkdir release-files
 mkdir release-files\ALVR
 xcopy /y ALVR\bin\Release\ALVR.exe release-files\ALVR
 xcopy /y ALVR\bin\Release\*.dll release-files\ALVR
-mkdir release-files\ALVR\driver
-mkdir release-files\ALVR\driver\bin
-mkdir release-files\ALVR\driver\bin\win64
-mkdir release-files\ALVR\driver\resources
-copy /y driver.vrdrivermanifest release-files\ALVR\driver
-copy /y driver_uninstall.bat release-files\ALVR\driver
-copy /y driver_install.bat release-files\ALVR\driver
-xcopy /y/s/e/i resources release-files\ALVR\driver\resources
-xcopy /y/s/e/i bin\win64\* release-files\ALVR\driver\bin\win64
+xcopy /y/s/e/i driver release-files\ALVR\driver

d7882d0005e143d59314b633ead1f7ae7a9880e6
polygraphene <polygraphene@gmail.com>
2018-05-30 13:42:23 +0900

    Prevent running multiple Launcher.

--- ALVR/Program.cs
+++ ALVR/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,22 @@ namespace ALVR
 {
     static class Program
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
+        private static readonly string mutexName = "Global\\ALVR_MUTEX_10C22CCD-9962-4F2F-9E9E-4251A32848C3";
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Launcher());
+            using (Mutex mutex = new Mutex(false, mutexName))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Already running!");
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Launcher());
+            }
         }
     }
 }

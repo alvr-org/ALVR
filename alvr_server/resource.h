c662c6a6bb0b7860b24f5dfffb21183b86807a7b
polygraphene <polygraphene@gmail.com>
2018-06-07 14:55:58 +0900

    v1.9.2

--- alvr_server/resource.h
+++ alvr_server/resource.h
@@ -5,7 +5,7 @@
 
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 9
-#define APP_VERSION_PATCH 1
+#define APP_VERSION_PATCH 2
 #define APP_VERSION_STRING__(major, minor, patch) #major "." #minor "." #patch
 #define APP_VERSION_STRING_(major, minor, patch) APP_VERSION_STRING__(major, minor, patch)
 #define APP_VERSION_STRING APP_VERSION_STRING_(APP_VERSION_MAJOR, APP_VERSION_MINOR, APP_VERSION_PATCH)

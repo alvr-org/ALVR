10c4b70f010012348a9d4e6dd4a92a614bcd900b
polygraphene <polygraphene@gmail.com>
2018-06-21 21:44:02 +0900

    Remove gop config to reduce burst of packet by IDR frame.

--- ALVR/ServerConfig.cs
+++ ALVR/ServerConfig.cs
@@ -130,7 +130,7 @@ namespace ALVR
                 driverConfig.controllerRenderModelName = "vr_controller_vive_1_5";
                 driverConfig.controllerSerialNumber = "ALVR Remote Controller";
 
-                driverConfig.nvencOptions = "-codec h264 -preset ll_hq -rc cbr_ll_hq -gop 120 -fps 60 -bitrate "
+                driverConfig.nvencOptions = "-codec h264 -preset ll_hq -rc cbr_ll_hq -fps 60 -bitrate "
                     + Properties.Settings.Default.bitrate + "M -maxbitrate " + Properties.Settings.Default.bitrate + "M";
 
                 driverConfig.renderWidth = Properties.Settings.Default.renderWidth;

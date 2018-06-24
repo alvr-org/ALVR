a54f52268b60cd8617a67071aefec1ed6c00bcd6
polygraphene <polygraphene@gmail.com>
2018-06-24 18:25:20 +0900

    Fix some string.

--- driver/driver_install.bat
+++ driver/driver_install.bat
@@ -1,7 +1,7 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
 "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%~dp0
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Driver Probably Installed', 10, 'Title!', 64 );close()"
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Driver Probably Installed', 10, 'Driver installer', 64 );close()"
 ) ELSE (
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed', 10, 'Title!', 64 );close()"
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed', 10, 'Driver installer', 64 );close()"
 )
--- driver/driver_uninstall.bat
+++ driver/driver_uninstall.bat
@@ -1,7 +1,7 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
 "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%~dp0
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Driver Removed', 10, 'Title!', 64 );close()"
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Driver Removed', 10, 'Driver installer', 64 );close()"
 ) ELSE (
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Removal Failed', 10, 'Title!', 64 );close()"
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Removal Failed', 10, 'Driver installer', 64 );close()"
 )

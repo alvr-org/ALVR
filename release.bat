d7660e164fa915d09f6ddcfba9017a4f27720167
polygraphene <polygraphene@gmail.com>
2018-07-02 03:31:31 +0900

    Fix release.bat

--- release.bat
+++ release.bat
@@ -1,5 +1,6 @@
 mkdir release-files
 mkdir release-files\ALVR
+mkdir release-files\ALVR\logs
 xcopy /y ALVR\bin\Release\ALVR.exe release-files\ALVR
 xcopy /y CrashReport\bin\Release\CrashReport.exe release-files\ALVR
 xcopy /y ALVR\bin\Release\*.dll release-files\ALVR
@@ -7,3 +8,7 @@ xcopy /y/s/e/i driver release-files\ALVR\driver
 xcopy /y libswresample\lib\*.dll release-files\ALVR\driver\bin\win64
 xcopy /y/s/e/i freepie-samples release-files\ALVR\freepie-samples
 xcopy /y ALVRFreePIE\bin\Release\ALVRFreePIE.dll release-files\ALVR
+
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" installer\installer.iss
+
+pause

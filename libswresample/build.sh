2f01eea6b54a6f75cb3078fcd62e07b64e82f3af
polygraphene <polygraphene@gmail.com>
2018-07-10 02:36:08 +0900

    Add FFmpeg license text.

--- libswresample/build.sh
+++ libswresample/build.sh
@@ -1,2 +1,9 @@
+# You need Ubuntu 16.04 to build ffmpeg libswresample
+# sudo apt install mingw-w64
+git clone https://git.ffmpeg.org/ffmpeg.git
+cd ffmpeg
+git checkout n4.0.1
 # build libswresample on linux mingw64
-./configure --arch=x86_64 --target-os=mingw64 --cross-prefix=x86_64-w64-mingw32- --disable-avdevice  --disable-avcodec --disable-avformat --disable-swscale --disable-postproc --disable-avfilter --enable-shared --disable-static
+./configure --arch=x86_64 --target-os=mingw64 --cross-prefix=x86_64-w64-mingw32- --disable-avdevice --disable-avcodec --disable-avformat --disable-swscale --disable-postproc --disable-avfilter --enable-shared --disable-static
+make -j4
+make install DESTDIR="$PWD/installdirs"

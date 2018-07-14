4c6829a4deeb8a059275cb37fa0c6101609aee7b
Carl Kenner <carl.kenner@gmail.com>
2018-07-15 00:34:05 +0930

    Fix issue #141 Windows Defender detecting the bat files as Trojan:Win32/Powessere.G
    Fixes issue #141.
    There is no virus. But this change prevents Windows Defender from falsely detecting one.
    The .bat files now look and behave like regular .bat files.

--- driver/driver_install.bat
+++ driver/driver_install.bat
@@ -1,7 +1,8 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
 "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%~dp0
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Driver Probably Installed', 10, 'Driver installer', 64 );close()"
+echo "Driver Probably Installed"
 ) ELSE (
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed', 10, 'Driver installer', 64 );close()"
+echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed"
 )
+pause

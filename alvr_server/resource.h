c0607aad820990cbd785f93f080879247cdc8b27
polygraphene <polygraphene@gmail.com>
2018-07-27 23:10:19 +0900

    v2.3.0

--- alvr_server/resource.h
+++ alvr_server/resource.h
@@ -5,9 +5,9 @@
 #define IDR_MESSAGE_BG_TEXTURE 1004
 
 #define APP_VERSION_MAJOR 2
-#define APP_VERSION_MINOR 2
-#define APP_VERSION_PATCH 3
-#define APP_VERSION_SUFFIX "-alpha"
+#define APP_VERSION_MINOR 3
+#define APP_VERSION_PATCH 0
+#define APP_VERSION_SUFFIX ""
 #define APP_VERSION_STRING__(major, minor, patch, suffix) #major "." #minor "." #patch "" suffix
 #define APP_VERSION_STRING_(major, minor, patch, suffix) APP_VERSION_STRING__(major, minor, patch, suffix)
 #define APP_VERSION_STRING APP_VERSION_STRING_(APP_VERSION_MAJOR, APP_VERSION_MINOR, APP_VERSION_PATCH, APP_VERSION_SUFFIX)

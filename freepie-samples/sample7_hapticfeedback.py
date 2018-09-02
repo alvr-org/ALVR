d8ed86d4c96eab025adf7f028e660ae10fab1f3c
ahinore <ahinore@gmail.com>
2018-09-02 17:03:18 +0900

    Add haptic feedback sample

---
+++ freepie-samples/sample7_hapticfeedback.py
@@ -0,0 +1,8 @@
+
+diagnostics.watch(alvr.input_haptic_feedback[0][0]) #fAmplitude
+diagnostics.watch(alvr.input_haptic_feedback[0][1]) #fDurationSeconds
+diagnostics.watch(alvr.input_haptic_feedback[0][2]) #fFrequency
+diagnostics.watch(alvr.input_haptic_feedback[1][0]) #fAmplitude
+diagnostics.watch(alvr.input_haptic_feedback[1][1]) #fDurationSeconds
+diagnostics.watch(alvr.input_haptic_feedback[1][2]) #fFrequency
+

e33a93e14a30094a70b1e25b228d6a46729522aa
polygraphene <polygraphene@gmail.com>
2019-05-24 17:56:19 +0900

    v2.4.0-alpha2

--- ALVR/Properties/AssemblyInfo.cs
+++ ALVR/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.4.0-alpha1")]
+[assembly: AssemblyInformationalVersion("2.4.0-alpha2")]

0bb1dc45990415fac1cc4d69d5afb393d9520b8b
JackD83 <maier.sebastian@gmail.com>
2020-06-22 14:07:36 +0200

    added i18n to monitor tab

--- server_release_template/web_gui/js/app/nls/monitor.js
+++ server_release_template/web_gui/js/app/nls/monitor.js
@@ -1,7 +1,21 @@
 define({
     "root": {
-        "title": "Logging",
-     
+        "connection": "Connection",
+        "logging": "Logging",
+        "clients": "Clients",
+        "newClients": "New Clients",
+        "trustedClients": "Trusted Clients",
+        "manAddClient": "Manually add client",
+        "statistics": "Streaming Statistics",
+        "totalPackets" : "Total Packets",
+        "packetRate": "PacketRate",
+        "encoderLatency" : "Encoder Latency",
+        "decoderLatency" : "Decoder Latency",
+        "addClient" : "Add client",
+        "clientType" : "Type",
+        "ipAddress" : "IP-Address",
+        "ipHelp" : "Enter know IP-address of the HMD",
+        "addClientButton" : "Add client" 
     },
     "de-de": false
 });

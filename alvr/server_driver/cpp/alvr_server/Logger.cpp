e06d664c8a42d5aeeaa6a66de17e1d89f7944846
JackD83 <maier.sebastian@gmail.com>
2020-07-19 09:20:29 +0200

    re-enabled driver log

--- alvr/server_driver/cpp/alvr_server/Logger.cpp
+++ alvr/server_driver/cpp/alvr_server/Logger.cpp
@@ -5,7 +5,9 @@ void _log(const char *format, va_list args, void (*logFn)(const char *))
 	char buf[1024];
 	vsnprintf(buf, sizeof(buf), format, args);
 	logFn(buf);
-	//DriverLogVarArgs(format, args);
+
+	//TODO: driver logger should concider current log level
+	DriverLogVarArgs(format, args);
 }
 
 void Log(const char *format, ...)

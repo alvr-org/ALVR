1544544c7bbd865843e651a204b5925ec28f25de
Ondřej Slabý <kron258@gmail.com>
2020-09-21 00:48:56 +0200

    Tag Rust Android log and set a log level for it

--- alvr/client/src/logging_backend.rs
+++ alvr/client/src/logging_backend.rs
@@ -1,6 +1,10 @@
 pub fn init_logging() {
     #[cfg(target_os = "android")]
-    android_logger::init_once(android_logger::Config::default());
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_tag("ALVR native-rust")
+            .with_min_level(log::Level::Info),
+    );
 
     crate::logging::set_panic_hook();
 }

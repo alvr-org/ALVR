7d2c95725f23e08c57634d2a970598a514f29069
zarik5 <riccardo.zaglia5@gmail.com>
2020-10-25 20:39:25 +0100

    Update version requirements

--- alvr/common/src/data/version.rs
+++ alvr/common/src/data/version.rs
@@ -6,8 +6,8 @@ pub const ALVR_NAME: &str = "ALVR";
 pub const ALVR_SERVER_VERSION: &str = env!("SERVER_VERSION");
 pub const ALVR_CLIENT_VERSION: &str = env!("CLIENT_VERSION");
 
-pub const ALVR_SERVER_VERSION_REQ: &str = ">=12.5.0";
-pub const ALVR_CLIENT_VERSION_REQ: &str = ">=12.5.0";
+pub const ALVR_SERVER_VERSION_REQ: &str = ">=12.5.0-pre.2";
+pub const ALVR_CLIENT_VERSION_REQ: &str = ">=12.5.0-pre.2";
 
 pub fn is_version_compatible(version: &str, requirement: &str) -> StrResult<bool> {
     let version = trace_err!(Version::parse(version))?;

f6a6e99eb7897f3cbbaef3600d062738b5376334
Jonathan Hecl <jonathanhecl@gmail.com>
2020-10-27 18:01:28 -0300

    Spanish (#336)
    * Fix
    
    * Key frames lang fix
    
    * New messages
    
    * New messages

--- server_release_template/web_gui/js/app/nls/es/main.js
+++ server_release_template/web_gui/js/app/nls/es/main.js
@@ -1,14 +1,17 @@
 define({
     "title": "ALVR",
     "monitor": "Monitor",
-    "settings": "Ajustes",   
+    "settings": "Ajustes",
     "installation": "Instalación",
     "about": "Información",
-    "toggleAdvanced" : "Configuración avanzada",
+    "toggleAdvanced": "Configuración avanzada",
     "runSetupWizard": "Iniciar asistente de configuración",
     "addFirewallRules": "Añadir excepción al cortafuegos",
     "removeFirewallRules": "Eliminar excepción del cortafuegos",
     "firewallSuccess": "Se ha modificado el cortafuegos con éxito",
     "restartSteamVR": "Reiniciar SteamVR",
-    "aboutText": "Transmite los juegos de RV desde tu PC a tu Oculus Quest a través de Wi-Fi. <br/> ALVR utiliza tecnologías como el Asynchronous Timewarp y Fixed Foveated Rendering para una experiencia más confortable. <br/> Todos los juegos diseñados para Oculus Rift(S) deberían funcionar con ALVR. <br/> Este es un fork de ALVR que sólo funciona con el Oculus Quest.  <br/> <br/> Visítenos en GitHub: <a  target='_blank' href='https://github.com/JackD83/ALVR/'>https://github.com/JackD83/ALVR/</a> <br/> Únete a nosotros en Discord: <a  target='_blank' href='https://discord.com/invite/KbKk3UM'>https://discord.com/invite/KbKk3UM</a>"
+    "aboutText": "Transmite los juegos de RV desde tu PC a tu Oculus Quest a través de Wi-Fi. <br/> ALVR utiliza tecnologías como el Asynchronous Timewarp y Fixed Foveated Rendering para una experiencia más confortable. <br/> Todos los juegos diseñados para Oculus Rift(S) deberían funcionar con ALVR. <br/> Este es un fork de ALVR que sólo funciona con el Oculus Quest.  <br/> <br/> Visítenos en GitHub: <a  target='_blank' href='https://github.com/JackD83/ALVR/'>https://github.com/JackD83/ALVR/</a> <br/> Únete a nosotros en Discord: <a  target='_blank' href='https://discord.com/invite/KbKk3UM'>https://discord.com/invite/KbKk3UM</a>",
+    "checkForUpdates": "Buscar actualizaciones",
+    "noNeedForUpdate": "No es necesario actualizar, ya tienes la última versión",
+    "needUpdateClickForMore": "Hay una nueva actualización, haga clic para obtener más información"
 });

a05069d03d8ac5581aeda94f64990af84d7fc73e
zarik5 <riccardo.zaglia5@gmail.com>
2020-10-27 21:54:27 +0100

    Refactoring

--- server_release_template/web_gui/js/app/setupWizard.js
+++ server_release_template/web_gui/js/app/setupWizard.js
@@ -7,7 +7,7 @@ define([
 ], function (_, driverList, wizardTemplate, i18n) {
     return function (alvrSettings) {
 
-        class GPUObj {
+        class GPU {
             constructor() {
                 let graphicsDeviceName = "";
                 $.ajax({
@@ -35,14 +35,14 @@ define([
             }
         }
 
-        function CheckGPUSupport(GPU) {
+        function CheckGPUSupport(gpu) {
             var unsupportedGPURegex = new RegExp("(Radeon (((VIVO|[2-9][0-9][0-9][0-9]) ?\S*)|VE|LE|X(1?[0-9][0-5]0))"+
                            "|GeForce ((8[3-9][0-9]|9[0-3][0-9]|94[0-5])[AM]|GT 1030|GTX 9([2-3][0-9]|40)MX|MX(110|130|1[5-9][0-9]|2[0-9][0-9]|3[0-2][0-9]|330|350|450)))")
 
-            switch (GPU.vendor) {
+            switch (gpu.vendor) {
                 case "NVIDIA":
                 case "AMD":
-                    if (unsupportedGPURegex.test(GPU.name)) {
+                    if (unsupportedGPURegex.test(gpu.name)) {
                         return '🔴 '+i18n.GPUUnsupported;
                     } else {
                         return '🟢 '+i18n.GPUSupported;
@@ -70,9 +70,9 @@ define([
                     keyboard: false
                 });
 
-                GPU = new GPUObj();
-                $("#GPU").text(GPU.fullName);
-                $("#GPUSupportText").text((CheckGPUSupport(GPU)));
+                const gpu = new GPU();
+                $("#GPU").text(gpu.fullName);
+                $("#GPUSupportText").text((CheckGPUSupport(gpu)));
 
                 $("#addFirewall").click(() => {
                     $.get("firewall-rules/add", undefined, (res) => {

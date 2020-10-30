912494d0ee3675965c887d204790514e85e87b8e
Nexite <nikhil@gargmail.com>
2020-10-30 13:29:29 -0700

    Started implementation of auto-updater
    refer to commit before when finalizing

--- alvr/launcher/src/main.rs
+++ alvr/launcher/src/main.rs
@@ -1,14 +1,18 @@
-#![windows_subsystem = "windows"]
+// #![windows_subsystem = "windows"]
 
-use alvr_common::{commands::*, *};
+use alvr_common::{*, data::ALVR_SERVER_VERSION, commands::*};
 use logging::{show_e, show_err};
 use serde_json as json;
-use std::{
-    env,
-    path::PathBuf,
-    thread,
-    time::{Duration, Instant},
-};
+use std::{env, fs::File, path::PathBuf, sync::Arc, process::Command, thread, time::{Duration, Instant}};
+#[macro_use]
+extern crate self_update;
+use self_update::cargo_crate_version;
+
+#[cfg(windows)]
+use std::os::windows::process::CommandExt;
+
+#[cfg(windows)]
+pub const CREATE_NO_WINDOW: u32 = 0x0800_0000;
 
 const SHUTDOWN_TIMEOUT: Duration = Duration::from_secs(10);
 
@@ -65,12 +69,12 @@ fn window_mode() -> StrResult {
     if mutex.is_single() {
         maybe_delete_alvr_dir_storage();
 
-        let html_content = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/html/index.html"));
-        let window = trace_err!(alcro::UIBuilder::new()
+        let html_content = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/html/index1.html"));
+        let window = Arc::new(trace_err!(alcro::UIBuilder::new()
             .content(alcro::Content::Html(html_content))
             .size(200, 200)
             .custom_args(&["--disk-cache-size=1"])
-            .run())?;
+            .run())?);
 
         trace_err!(window.bind("checkSteamvrInstallation", |_| {
             Ok(json::Value::Bool(check_steamvr_installation()))
@@ -97,24 +101,84 @@ fn window_mode() -> StrResult {
             Ok(json::Value::Null)
         }))?;
 
-        trace_err!(window.eval("init()"))?;
+        
+        trace_err!(window.bind("update", { let window = window.clone();  move |_| {
+            println!("{}", ALVR_SERVER_VERSION.to_string());
+            update();
+            println!("updated!");
+            // drop(mutex);
+            window.close();
+            println!("restarting!");
+
+            // reopen alvr
+            let mut command = Command::new(::std::env::current_dir().unwrap().join("ALVR launcher"));
+            command.spawn().ok();
+            
+            Ok(json::Value::Null)
+        }}))?;
+
+        // trace_err!(window.eval("init()"))?;
 
         window.wait_finish();
-
+        
         // This is needed in case the launcher window is closed before the driver is loaded,
         // otherwise this does nothing
-        apply_driver_paths_backup(current_alvr_dir()?)?;
+        // apply_driver_paths_backup(current_alvr_dir()?)?;
+        
     }
     Ok(())
 }
 
 fn main() {
+    println!("launching ALVR");
     let args = env::args().collect::<Vec<_>>();
-
     match args.get(1) {
         Some(flag) if flag == "--restart-steamvr" => restart_steamvr(),
         _ => {
             show_err(window_mode()).ok();
         }
     }
+}
+
+fn update() -> Result<(), Box<::std::error::Error>> {
+    let releases = self_update::backends::github::ReleaseList::configure()
+        .repo_owner("Nexite")
+        .repo_name("ALVR")
+        .build()?
+        .fetch()?;
+    println!("found releases:");
+    println!("{:#?}\n", releases);
+
+    // get the first available release
+    let asset = releases[0]
+        .asset_for("autoupdater").unwrap();
+    println!("{:#?}\n", asset);
+    println!("test1");
+    let tmp_dir = tempfile::Builder::new()
+            .prefix("self_update")
+            .tempdir_in(::std::env::temp_dir())?;
+    println!("test2");
+    let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+    println!("test3");
+    println!("{}", tmp_tarball_path.to_str().unwrap());
+    let tmp_tarball = File::create(&tmp_tarball_path)?;
+    println!("test4");
+
+    self_update::Download::from_url(&asset.download_url)
+        .show_progress(true)
+        .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+        .download_to(&tmp_tarball)?;
+
+    let bin_name = std::path::PathBuf::from("ALVR launcher.exe");
+    self_update::Extract::from_source(&tmp_tarball_path)
+        .archive(self_update::ArchiveKind::Zip)
+        .extract_file(&tmp_dir.path(), &bin_name)?;
+
+    let tmp_file = tmp_dir.path().join("replacement_tmp");
+    let bin_path = tmp_dir.path().join(bin_name);
+    self_update::Move::from_source(&bin_path)
+        .replace_using_temp(&tmp_file)
+        .to_dest(&::std::env::current_exe()?)?;
+    println!("{}", ALVR_SERVER_VERSION.to_string());
+    Ok(())
 }

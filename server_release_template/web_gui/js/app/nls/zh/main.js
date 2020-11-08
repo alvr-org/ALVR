9f7601ab9095c169e32ca825f22b26c36d6b900a
zarik5 <riccardo.zaglia5@gmail.com>
2020-11-08 12:51:43 +0100

    Simplified Chinese translation update

--- server_release_template/web_gui/js/app/nls/zh/main.js
+++ server_release_template/web_gui/js/app/nls/zh/main.js
@@ -10,5 +10,8 @@ define({
     "removeFirewallRules": "移除防火墙规则",
     "firewallSuccess": "防火墙规则设置成功",
     "restartSteamVR": "重启SteamVR",
-    "aboutText": "通过Wi-FI将VR游戏从PC传输到Oculus Quest。 <br/> ALVR使用诸如“异步时间扭曲”和“固定凹面渲染”之类的技术来获得更流畅的体验。<br/> 所有与Oculus Rift兼容的游戏都应与ALVR兼容。 <br/> 这是仅限用于Oculus Quest的ALVR的分支项目。<br/> <br/> 官方GitHub：<a  target='_blank' href='https://github.com/JackD83/ALVR/'>https://github.com/JackD83/ALVR/</a> <br/> 官方Discord频道：<a target='_blank' href='https://discord.com/invite/KbKk3UM'>https://discord.com/invite/KbKk3UM</a> <br/> <br/>程序汉化：k7Ysh5A_4.1."
-});
+    "aboutText": "通过Wi-FI将VR游戏从PC传输到Oculus Quest。 <br/> ALVR使用诸如“异步时间扭曲”和“固定凹面渲染”之类的技术来获得更流畅的体验。<br/> 所有与Oculus Rift兼容的游戏都应与ALVR兼容。 <br/> 这是仅限用于Oculus Quest的ALVR的分支项目。<br/> <br/> 官方GitHub：<a  target='_blank' href='https://github.com/JackD83/ALVR/'>https://github.com/JackD83/ALVR/</a> <br/> 官方Discord频道：<a target='_blank' href='https://discord.com/invite/KbKk3UM'>https://discord.com/invite/KbKk3UM</a> <br/> <br/>程序汉化：k7Ysh5A_4.1.",
+    "checkForUpdates": "检测更新",
+    "noNeedForUpdate": "已更新到最新版本",
+    "needUpdateClickForMore": "检测到新版本更新，点击获得更多信息"
+	});

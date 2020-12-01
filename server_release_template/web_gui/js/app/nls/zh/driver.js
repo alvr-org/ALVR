0239a0f0d9d5554f71bd23869622156d389088e1
zarik5 <riccardo.zaglia5@gmail.com>
2020-12-01 13:50:16 +0100

    Simplified Chinese update Update licence and crate authors

--- server_release_template/web_gui/js/app/nls/zh/driver.js
+++ server_release_template/web_gui/js/app/nls/zh/driver.js
@@ -5,5 +5,6 @@ define({
     "driverUnregisterSuccessful": "已注册的VR驱动程序成功移除",
     "driverUnregisterFailed": "移除已注册VR驱动程序失败",
     "registerAlvrDriver": "注册ALVR驱动程序",
+    "driverNotice": "重要内容更新：从ALVR v13.1.0开始，不建议注册ALVR驱动程序。现在，只有当你想单独启动SteamVR或者当你想将ALVR与其他SteamVR驱动一起使用时，才需要注册驱动，比如<a href='https://www.driver4vr.com/'>Driver4VR</a>用于全身追踪。",
     "registerAlvrDriverSuccess": "已成功注册ALVR驱动程序"
 });

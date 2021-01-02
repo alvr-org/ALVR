cdac650f12c7efcb4efe26b59cda43afe197f7b0
Chris <aidasan@hotmail.fr>
2021-01-02 20:59:04 +0900

    Add language selection

--- server_release_template/web_gui/js/app/main.js
+++ server_release_template/web_gui/js/app/main.js
@@ -23,10 +23,21 @@ define([
 
         $("#bodyContent").append(template);
         $(document).ready(() => {
-            $('#loading').remove();
+            $("#loading").remove();
 
             try {
                 var settings = new Settings();
+                
+                // update the current language on startup
+                let storedLocale = localStorage.getItem("i18nLocale");
+                let sessionLocale = session.i18nLocale;
+                $("#i18nLocale").val(storedLocale);
+                if (sessionLocale !== storedLocale) {
+                    storedLocale = sessionLocale;
+                    localStorage.setItem("i18nLocale", storedLocale);
+                    window.location.reload();
+                }
+
                 var wizard = new SetupWizard(settings);
                 var monitor = new Monitor(settings);
             } catch (error) {
@@ -36,7 +47,7 @@ define([
                     delayIndicator: false,
                     sound: false,
                     position: "bottom left",
-                    iconSource: 'fontAwesome',
+                    iconSource: "fontAwesome",
                     msg: error.stack,
                     closable: true,
                     messageHeight: 250,
@@ -101,13 +112,13 @@ define([
                             onClick: function(){
                                 $.ajax({
                                     headers: { 
-                                        'Accept': 'application/json',
-                                        'Content-Type': 'application/json' 
+                                        "Accept": "application/json",
+                                        "Content-Type": "application/json" 
                                     },
-                                    'type': 'POST',
-                                    'url': "/open",
-                                    'data': JSON.stringify("https://github.com/alvr-org/ALVR/releases/latest"),
-                                    'dataType': 'JSON'
+                                    "type": "POST",
+                                    "url": "/open",
+                                    "data": JSON.stringify("https://github.com/alvr-org/ALVR/releases/latest"),
+                                    "dataType": "JSON"
                                 })
                             }
                         })
@@ -115,12 +126,17 @@ define([
                 })
             })
 
+            $("#i18nLocale").change(() => {
+                storedLocale = $("#i18nLocale").val();
+                localStorage.setItem("i18nLocale", storedLocale);
+                session.i18nLocale = storedLocale;
+                settings.storeSession("other");
+                window.location.reload();
+            })
+
             $("#version").text("v" + version);
 
             driverList.fillDriverList("registeredDriversInst");
-
-            
-
 
         });
     });

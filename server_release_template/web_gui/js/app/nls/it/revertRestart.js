8284888219ca2b041dd31aa8607ed451d0f539e8
Chris <aidasan@hotmail.fr>
2021-01-11 21:50:33 +0900

    Italian checked

--- server_release_template/web_gui/js/app/nls/it/revertRestart.js
+++ server_release_template/web_gui/js/app/nls/it/revertRestart.js
@@ -1,9 +1,10 @@
 define({
     // Revert Popup
-    "confirmRevertTitle" : "Reimpostare valore?",
-    "okRevertButton":"OK",
-    "cancelRevertButton" : "Annulla",
-    "revertText" : "Ripristinare impostazione al suo valore predefinito, cioè ",
+    "confirmRevertTitle": "Reimpostare valore?",
+    "okRevertButton": "OK",
+    "cancelRevertButton": "Annulla",
+    "revertText": "Ripristinare impostazione al suo valore predefinito, cioè ",
+    // Restart Popup
     "confirmRestartTitle": "Riavviare SteamVR?",
     "restartText": "Riavviare SteamVR? Questo chiuderà l'app/gioco aperto al momento",
     "dontAskRestart": "Non chiedere più",

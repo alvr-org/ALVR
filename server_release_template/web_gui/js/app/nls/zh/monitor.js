130d1ea0f5d7bbaac07d55d1fc3b6e51a3ff07a3
Chris <aidasan@hotmail.fr>
2021-01-11 14:48:10 +0900

    Chinese checked

--- server_release_template/web_gui/js/app/nls/zh/monitor.js
+++ server_release_template/web_gui/js/app/nls/zh/monitor.js
@@ -1,15 +1,31 @@
 define({
+    // Connection tab
     "connection": "客户端连接",
-    "logging": "日志记录",
+    // Clients container
     "clients": "客户端",
     "newClients": "可连接的新客户端",
+    // "addTrustedClient": "",
     "trustedClients": "已信任的客户端",
+    // "removeTrustedClient": "",
+    "troubleshooting": "你的头显没有显示在这里？访问我们的<a href='https://github.com/alvr-org/ALVR/wiki/Troubleshooting#alvr-cant-see-my-headset'>Github问题解决页面</a>以获得帮助。",
+    // Manually add client page
     "manAddClient": "手动添加客户端",
-    "statistics": "串流状态信息",
     "addClient": "添加客户端",
+    // "deviceName": "",
+    // "deviceNameHelp": "",
+    // "clientHostname": "",
+    // "clientHostnameHelp": "",
     "ipAddress": "IP地址",
     "ipHelp": "添加已知的头显IP地址",
     "addClientButton": "添加客户端",
+    // Configure client page
+    // "configureClient": "",
+    // "configureClientButton": "",
+    // "configureClientAddIp": "",
+    // "configureClientRemoveIp": "",
+    // Statistics container
+    "statistics": "状态信息",
+    "streamingStatistics": "串流状态信息",
     "totalPackets": "数据包总数",
     "packetRate": "封包速率",
     "packetsLostTotal": "丢包数",
@@ -28,5 +44,19 @@ define({
     "serverFPS": "服务端帧数",
     "packets": "个数据包",
     "packetss": "个数据包每秒",
-	"troubleshooting": `你的头显没有显示在这里？访问我们的<a  href="https://github.com/alvr-org/ALVR/wiki/Troubleshooting#alvr-cant-see-my-headset">Github问题解决页面</a>以获得帮助。`
+    // Logging tab
+    "logging": "日志记录",
+    // validation errors
+    // "error_DuplicateHostname": "",
+    // "error_DuplicateIp": "",
+    // "error_InvalidIp": "",
+    // Performance graphs tab
+    // "performanceGraphs": "",
+    // "performanceEncode": "",
+    // "performanceDecode": "",
+    // "performanceTransport": "",
+    // "performanceOther": "",
+    // "performanceLatency": "",
+    // "performanceFramerate": "",
+    // "performanceNoDateCaptured": "",
 });

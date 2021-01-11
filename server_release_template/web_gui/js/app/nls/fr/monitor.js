dbf9b60a6ce13c8e96c6f9fb8ce21433a3ecb56a
Chris <aidasan@hotmail.fr>
2021-01-11 21:59:29 +0900

    French checked

--- server_release_template/web_gui/js/app/nls/fr/monitor.js
+++ server_release_template/web_gui/js/app/nls/fr/monitor.js
@@ -1,16 +1,31 @@
 define({
+    // Connection tab
     "connection": "Connexion",
-    "logging": "Logs",
+    // Clients container
     "clients": "Client",
     "newClients": "Nouveau client",
+    "addTrustedClient": "Ajouter",
     "trustedClients": "Client approuvés",
-    "manAddClient": "Ajouter un client manuellment",
     "removeTrustedClient": "Retirer",
-    "statistics": "Statistiques de streaming",
+    "troubleshooting": "Votre casque n'apparait pas dans la liste ? Visitez note page de support <a href='https://github.com/alvr-org/ALVR/wiki/Troubleshooting#alvr-cant-see-my-headset'>ici</a>",
+    // Manually add client page
+    "manAddClient": "Ajouter un client manuellement",
     "addClient": "Ajouter client",
+    "deviceName": "Nom du casque",
+    "deviceNameHelp": "Donnez au casque un nom",
+    "clientHostname": "Nom d'hôte",
+    "clientHostnameHelp": "Le même que celui affiché sur votre casque quand le client est lancé",
     "ipAddress": "Adresse IP",
     "ipHelp": "Entrez l'adresse IP connue du casque",
     "addClientButton": "Ajouter client",
+    // Configure client page
+    "configureClient": "Configuration du client",
+    "configureClientButton": "Configurer",
+    "configureClientAddIp": "Ajouter une adresse IP",
+    "configureClientRemoveIp": "Supprimer",
+    // Statistics container
+    "statistics": "Statistiques",
+    "streamingStatistics": "Statistiques de streaming",
     "totalPackets": "Paquets totaux",
     "packetRate": "Taux de paquets",
     "packetsLostTotal": "Paquets perdus totaux",
@@ -27,6 +42,21 @@ define({
     "fecFailureInSecond": "Erreurs FEC par seconde",
     "clientFPS": "FPS du client",
     "serverFPS": "FPS du serveur",
-    "packets" : "Paquets",
-    "packetss" : "Paquets par seconde"
+    "packets": "Paquets",
+    "packetss": "Paquets par seconde",
+    // Logging tab
+    "logging": "Logs",
+    // validation errors
+    "error_DuplicateHostname": "Ce nom d'hôte est déjà utilisé par un autre casque.",
+    "error_DuplicateIp": "Cette addresse IP est déjà utilisé par un autre casque.",
+    "error_InvalidIp": "Cette addresse IP n'est par valide.",
+    // Performance graphs tab
+    "performanceGraphs": "Graphiques",
+    "performanceEncode": "Encodeur",
+    "performanceDecode": "Décodeur",
+    "performanceTransport": "Transport",
+    "performanceOther": "Autre",
+    "performanceLatency": "Latence",
+    "performanceFramerate": "Fréquence d'images",
+    "performanceNoDateCaptured": "Aucune donnée capturée pour le moment",
 });
--- server_release_template/web_gui/js/app/nls/fr/wizard.js
+++ server_release_template/web_gui/js/app/nls/fr/wizard.js
@@ -1,28 +1,40 @@
 define({
+    // Banner
     "title": "Bienvenue sur ALVR",
     "subtitle": "Cet assistant va vous aider pour la configuration de base d'ALVR",
+    // Hardware page
     "titleHardwareReq": "Matériel requis",
-    "textHardwareReq": "ALVR nécésite une carte graphique dédié et recente (Intel HD graphics non supporté). Voici une <a href='https://github.com/polygraphene/ALVR/wiki/Supported-GPU'>liste de GPU supporté</a>. <br/> <br/> Assurez vous d'avoir au moins une sortie audio.",
+    "textHardwareReq": "ALVR nécésite une carte graphique dédié et recente (Intel HD graphics non supporté). Voici une <a href='https://github.com/polygraphene/ALVR/wiki/Supported-GPU'>liste de GPU supporté</a>. <br/> <br/> Assurez vous d'avoir au moins une sortie audio. <br/> <br/> ",
+    // "YourGPUIs": "",
+    // "GPUSupported": "",
+    // "GPUUnsupported": "",
+    // Software page
     "titleSoftwareReq": "Logiciels requis",
-    "textSoftwareReq": "Pour transemettre l'audio du microphone veuillez installer <a  href='https://www.vb-audio.com/Cable/'>VB-Audio Virtual Cable</a>.",
+    "textSoftwareReq": "Pour transemettre l'audio du microphone veuillez installer <a href='https://www.vb-audio.com/Cable/'>VB-Audio Virtual Cable</a>.",
+    // Firewall page
     "titleFirewall": "Pare-feu",
     "textFirewall": "Pour communiquer avec le casque, quelques règles de pare-feu doivent etre ajoutés. Cela nécésite les droits administrateur.",
-    "buttonFirewall" : "Ajouter règles de pare-feu",
-    "titleTracking" : "Suivi",
-    "textTracking" : `Comment doit être le suivi des controlleurs. Si vous prévoyez de jouer a des jeux rapides tels que Beatsaber, choisez Moyen ou Rapide. Pour des jeux plus lents tels que Skyrim laissez le en Normal.`,
-    "normalTracking" : "Normal",
-    "mediumTracking" :"Moyen",
-    "fastTracking" : "Rapide",
-    "titlePerformance" : "Préréglages de performance",
-    "textPerformance" : "Choisisez le réglage qui correspond a votre ordinateur. Cela va ajuster quelques reglages pour vous.",
-    "compatPerformance" :"Compatibilité",
-    "qualityPerformance" : "Qualité visuelle",
+    "buttonFirewall": "Ajouter règles de pare-feu",
+    "firewallFailed": "Erreur d'ajout des règles du pare-feu",
+    "firewallSuccess": "Règles du pare-feu ajouté avec succes",
+    // Tracking page
+    "titleTracking": "Suivi",
+    "textTracking": `Comment doit être le suivi des controlleurs. Si vous prévoyez de jouer a des jeux rapides tels que Beatsaber, choisez Moyen ou Rapide. Pour des jeux plus lents tels que Skyrim laissez le en Normal.`,
+    // "oculusTracking": "",
+    "normalTracking": "Normal",
+    "mediumTracking": "Moyen",
+    "fastTracking": "Rapide",
+    // Performance page
+    "titlePerformance": "Préréglages de performance",
+    "textPerformance": "Choisisez le réglage qui correspond a votre ordinateur. Cela va ajuster quelques reglages pour vous.",
+    "compatPerformance": "Compatibilité",
+    "qualityPerformance": "Qualité visuelle",
+    // Import page
+    // "titleImport": "",
+    // "textImport": "",
+    // End page
     "titleFinished": "Fini",
     "textFinished":`Vous pouvez toujours recommencer cet assistant depuis la catégorie "Installation" a gauche`,
-    "buttonNext" :"Suivant",
-    "buttonClose" :"Fermer",
-    "driverFailed" :"Le driver n'a pas pu s'enregistrer",
-    "driverSuccess" :"Le driver s'est bien enregistrer",
-    "firewallFailed" : "Erreur d'ajout des règles du pare-feu",
-    "firewallSuccess" : "Règles du pare-feu ajouté avec succes"
+    "buttonNext": "Suivant",
+    "buttonClose": "Fermer",
 });

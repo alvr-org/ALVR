d1fb7723ea8485efd30341f4c7dea57f722296fa
Noel-Sa <67551311+Noel-Sa@users.noreply.github.com>
2021-02-02 17:58:16 +0100

    Update Translations
    Added entire "settings.js" and tweaked/completed others

--- server_release_template/dashboard/js/app/nls/de/driver.js
+++ server_release_template/dashboard/js/app/nls/de/driver.js
@@ -1,10 +1,10 @@
 define({
-    "noDrivers": "Keine Treiber registriert",
-    "registeredDrivers": "Registrierte Treiber",
-    "removeDriver": "Entfernen",
-    "driverUnregisterSuccessful": "Treiber erfolgreich Entfernt",
-    "driverUnregisterFailed": "Treiber konnte nicht entfernt werden",
-    "registerAlvrDriver": "ALVR Treiber hinzufügen",
-    "registerAlvrDriverSuccess": "Treiber erfolgreich hinzugefügt",
-    // "driverNotice": "",
+	"registeredDrivers": "Registrierte Treiber",
+	"noDrivers": "Keine Treiber registriert",
+	"removeDriver": "Entfernen",
+	"driverUnregisterSuccessful": "Treiber erfolgreich Entfernt",
+	"driverUnregisterFailed": "Treiber konnte nicht entfernt werden",
+	"registerAlvrDriver": "ALVR Treiber hinzufügen",
+	"registerAlvrDriverSuccess": "Treiber erfolgreich hinzugefügt",
+	"driverNotice": "WICHTIG: Den Treiber hinzuzufügen ist generell eine schlechte Idee. Inzwischen muss man den Treiber nur noch installieren, wenn man nur SteamVR öffnen will, oder du weitere SteamVR Treiber außer ALVR verwenden willst, wie <a href='https://www.driver4vr.com/'>Driver4VR</a> für 'full body tracking'.",
 });
--- server_release_template/dashboard/js/app/nls/de/wizard.js
+++ server_release_template/dashboard/js/app/nls/de/wizard.js
@@ -1,41 +1,43 @@
 define({
-    // Banner
+	//Banner
     "title": "Wilkommen zu ALVR",
     "subtitle": "Hier wirst du durchs Standard-Setup von ALVR geleitet",
-    // Hardware page
+	//Hardware page
     "titleHardwareReq": "Benötigte Hardware",
-    "textHardwareReq": "ALVR benötigt eine dedicated und aktuelle Grafikkarte (Intel HD Graphics ist nicht unterstützt). Hier ist die <a href='https://github.com/polygraphene/ALVR/wiki/Supported-GPU'>Liste von Unterstützen GPUs</a>. <br/> <br/> Stelle sicher, dass du mindestens ein Audio-Output-Gerät hast. <br/> <br/> ",
-    // "YourGPUIs": "",
-    // "GPUSupported": "",
-    // "GPUUnsupported": "",
-    // Software page
+    "textHardwareReq": "ALVR benötigt eine dedicated und aktuelle Grafikkarte (Intel HD Graphics ist nicht unterstützt). Hier ist die <a href='https://github.com/polygraphene/ALVR/wiki/Supported-GPU'>Liste von Unterstützen GPUs</a>. <br/> <br/> Stelle sicher, dass du mindestens ein Audio-Output-Gerät hast.",
+	//Software page
     "titleSoftwareReq": "Benötigte Software",
-    "textSoftwareReq": "Um das Quest Mikrofon zu streamen musst du das <a href='https://www.vb-audio.com/Cable/'>VB-Audio Virtual Cable installieren</a>.",
-    // Firewall page
-    "titleFirewall": "Firewall",
+    "textSoftwareReq": "Um das Quest Mikrofon zu streamen musst du das <a  href='https://www.vb-audio.com/Cable/'>VB-Audio Virtual Cable installieren</a>.",
+    //Firewall page
+	"titleFirewall": "Firewall",
     "textFirewall": "Um sich mit dem Headset verbinden zu können, müssen Firewall-Regeln hinzugefügt werden. Das benötigt Administratoren-Rechte.",
-    "buttonFirewall": "Firewall-Regeln hinzufügen",
     "firewallFailed": "Firewall-Regel konnte nicht erstellt werden",
-    "firewallSuccess": "Firewall-Regel erfolgreich erstellt",
-    // Tracking page
+    "firewallSuccess": "Firewall-Regel erfolgreich erstellt"
+	//Tracking page
+	"buttonFirewall": "Firewall-Regeln hinzufügen",
+    "buttonDriver": "ALVR-Treiber hinzufügen",
     "titleTracking": "Tracking",
     "textTracking": "Wie soll das Tracking der Controller gehandhabt werden.Wenn du planst schnelle Spiele wie Beatsaber zu spielen, wähle Mittel oder Schnell aus. Für langsamere Spiele wie Skyrim reicht die Einstellung Normal.",
-    // "oculusTracking": "",
-    "normalTracking": "Normal",
+    "oculusTracking": "Oculus Vorhersage",
+	"normalTracking": "Normal",
     "mediumTracking": "Mittel",
     "fastTracking": "Schnell",
-    // Performance page
+	//Performance page
     "titlePerformance": "Performance Voreinstellung",
     "textPerformance": "Bitte wähle eine Voreinstellung aus, die zu deinem Setup passt. Das wird ein paar Einstellungen für dich erledigen.",
     "compatPerformance": "Kompatibilität",
     "qualityPerformance": "Visuelle Qualität",
-    // Import page
-    // "titleImport": "",
-    // "textImport": "",
-    // End page
-    "titleFinished": "Fertiggestellt",
+    //Import page
+	"titleImport": "ALVR Preset importieren",
+	"textImport": `Du kannst Einstellungen oder Presets importieren (.json):
+        <ul>
+            <li> Presets für ein spezielles Headset. Dies ist für die <b>Oculus Go</b> (suche nach <code>oculus_go_preset.json</code> im Installationsordner).
+            <li> Einstellungen einer vorherigen ALVR Installation(<code>session.json</code>).
+        <ul>`,
+	//End page
+	"titleFinished": "Fertiggestellt",
     "textFinished": "Du kannst dieses Setup immer wieder von dem \"Installation\" Tab auf der linken Seite ausführen",
-    // "buttonBack": "",
     "buttonNext": "Weiter",
     "buttonClose": "Schließen",
+
 });

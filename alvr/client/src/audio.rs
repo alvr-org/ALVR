a17e2dccfd521f5b1d764a25eda6f68307d1ca50
zarik5 <riccardo.zaglia5@gmail.com>
2021-02-12 14:45:21 +0100

    Click removal test 2

--- alvr/client/src/audio.rs
+++ alvr/client/src/audio.rs
@@ -7,10 +7,11 @@ use cpal::Sample;
 use oboe::*;
 use parking_lot::Mutex;
 use std::{
+    cmp,
     collections::VecDeque,
     mem,
     sync::{
-        atomic::{AtomicUsize, Ordering},
+        atomic::{AtomicBool, Ordering},
         mpsc as smpsc, Arc,
     },
     thread,
@@ -80,28 +81,18 @@ pub async fn record_audio_loop(sample_rate: u32, sender: StreamSender<()>) -> St
     Ok(())
 }
 
-// A decreasing exponential used to reduce pop sound on packet loss. The pop sound is caused by a
-// jump in the sample values (to zero). An exponential curve gradually lowers the value to zero. The
-// exponential curve is the only curve (other than the trivial case y=k) that has no oscillatory
-// part and it gets perceived as silence. Any other curve (including a steadily decreasing line) CAN
-// be decomposed into sine waves, at least partially.
-//
-// Exponential multiplier: ratio between consecutive samples, used to construct the exponential.
-const EXPONENIAL_MULTIPLIER: f32 = 0.9995;
-
 struct PlayerCallback {
-    sample_buffer: Arc<Mutex<VecDeque<(f32, f32)>>>,
-    buffer_range_multiplier: usize,
-    last_packet_samples_count: Arc<AtomicUsize>,
+    // bucket where frames are normally read from
+    frame_buffer: Arc<Mutex<VecDeque<(f32, f32)>>>,
 
-    // decreasing exponential multiplier OR 1 - sample volume multiplier
-    multiplier: f32,
-
-    // starting point to construct the decreasing exponential
-    exponential_last_value: (f32, f32),
+    // frames read in case frame_buffer has drained
+    last_packet: Arc<Mutex<Option<Vec<(f32, f32)>>>>,
 
     // last played sample, used to restart the exponential if needed
-    last_sample_value: (f32, f32),
+    last_frame: (f32, f32),
+
+    // no audio samples to play or recovering
+    disrupted: Arc<AtomicBool>,
 }
 
 impl AudioOutputCallback for PlayerCallback {
@@ -110,65 +101,105 @@ impl AudioOutputCallback for PlayerCallback {
     fn on_audio_ready(
         &mut self,
         _: &mut dyn AudioOutputStreamSafe,
-        frames: &mut [(f32, f32)],
+        out_frames: &mut [(f32, f32)],
     ) -> DataCallbackResult {
-        let mut sample_buffer_ref = self.sample_buffer.lock();
+        let mut frame_buffer_ref = self.frame_buffer.lock();
 
-        // todo: use smarter policy with EventTiming
-        let last_packet_samples_count = self.last_packet_samples_count.load(Ordering::Relaxed);
-        if sample_buffer_ref.len() > 2 * self.buffer_range_multiplier * last_packet_samples_count {
-            error!("draining audio buffer. size: {}", sample_buffer_ref.len());
-
-            let buffer_size = sample_buffer_ref.len();
-            sample_buffer_ref
-                .drain(0..(buffer_size - self.buffer_range_multiplier * last_packet_samples_count));
-
-            // Restart decreasing exponential
-            self.multiplier = EXPONENIAL_MULTIPLIER;
-            self.exponential_last_value = (
-                self.last_sample_value.0 * EXPONENIAL_MULTIPLIER,
-                self.last_sample_value.1 * EXPONENIAL_MULTIPLIER,
-            );
-        }
-
-        let mut should_restart_exponential = false;
-        if sample_buffer_ref.len() >= frames.len() {
-            let buffer_it = sample_buffer_ref.drain(0..frames.len()).collect::<Vec<_>>();
-            frames.copy_from_slice(&buffer_it);
+        let got_samples = frame_buffer_ref.len() >= out_frames.len();
+        if got_samples {
+            let frames = frame_buffer_ref
+                .drain(0..out_frames.len())
+                .collect::<Vec<_>>();
+            out_frames.copy_from_slice(&frames);
         } else {
-            error!("audio buffer too small! size: {}", sample_buffer_ref.len());
-
-            // Restart decreasing exponential
-            should_restart_exponential = true;
-            self.exponential_last_value = (
-                self.last_sample_value.0 * EXPONENIAL_MULTIPLIER,
-                self.last_sample_value.1 * EXPONENIAL_MULTIPLIER,
-            );
+            error!("audio buffer too small! size: {}", frame_buffer_ref.len());
 
             // Clear buffer. Previous audio samples don't get cleared automatically and they cause
             // buzzing
-            frames.fill((0., 0.));
+            out_frames.fill((0., 0.));
+
+            self.disrupted.store(true, Ordering::Relaxed);
         }
 
-        // mix steadily decreasing exponential with samples with increasing volume (with a
-        // saturating exponential)
-        for (left, right) in frames.iter_mut() {
-            *left = (self.exponential_last_value.0 + *left * (1. - self.multiplier)).clamp(-1., 1.);
-            *right = self.exponential_last_value.1 + *right * (1. - self.multiplier).clamp(-1., 1.);
-
-            self.multiplier *= EXPONENIAL_MULTIPLIER;
-            self.exponential_last_value = (
-                self.exponential_last_value.0 * EXPONENIAL_MULTIPLIER,
-                self.exponential_last_value.1 * EXPONENIAL_MULTIPLIER,
+        if self.disrupted.load(Ordering::Relaxed) {
+            error!(
+                "distupted! got_samples {}, last_frame {:?}",
+                got_samples, self.last_frame
             );
+
+            // steadily increasing volume
+            if got_samples {
+                error!("fade in");
+                let frames_count = out_frames.len() as f32;
+                for (i, (left, right)) in out_frames.iter_mut().enumerate() {
+                    let multiplier = i as f32 / frames_count;
+                    *left *= multiplier;
+                    *right *= multiplier;
+                }
+
+                self.disrupted.store(false, Ordering::Relaxed);
+            }
+
+            if !got_samples || self.last_frame != (0., 0.) {
+                if let Some(packet) = &*self.last_packet.lock() {
+                    if packet.len() > out_frames.len() {
+                        // find sample in the old packet that most closely matches the last played
+                        // sample, making sure there will be enough samples to play after that
+                        let maybe_index = packet[0..packet.len() - out_frames.len()]
+                            .iter()
+                            .enumerate()
+                            .map(|(i, (l, r))| {
+                                (
+                                    i,
+                                    (self.last_frame.0 - l).abs() + (self.last_frame.1 - r).abs(),
+                                )
+                            })
+                            .min_by(|(_, dist1), (_, dist2)| {
+                                // f32 does not implement Ord, so no cmp method
+                                if dist1 < dist2 {
+                                    cmp::Ordering::Less
+                                } else {
+                                    cmp::Ordering::Greater
+                                }
+                            })
+                            .map(|(i, _)| i);
+
+                        if let Some(idx) = maybe_index {
+                            // Calculate volume scaling to perfectly match the choosen frame. Very
+                            // often the resulting values will be close to 1.
+                            // Note: volume scaling is preferred to a bias. DC bias is removed by
+                            // the audio controller and will still cause a pop. This is why a
+                            // decreasing exponential bias will not work.
+                            let mut starting_frame = packet[idx];
+                            if starting_frame.0.abs() < f32::EPSILON {
+                                starting_frame.0 = f32::EPSILON;
+                            }
+                            if starting_frame.1.abs() < f32::EPSILON {
+                                starting_frame.1 = f32::EPSILON;
+                            }
+                            let left_scale =
+                                (self.last_frame.0 / starting_frame.0).clamp(-1.2, 1.2);
+                            let right_scale = self.last_frame.1 / starting_frame.1.clamp(-1.2, 1.2);
+                            // error!("starting_frame {:?}", starting_frame);
+                            // error!("last_frame {:?}", self.last_frame);
+                            error!("scale {} {}", left_scale, right_scale);
+
+                            let frames_count = out_frames.len() as f32;
+                            for (idx, (left, right)) in out_frames.iter_mut().enumerate() {
+                                let multiplier = 1. - (idx + 1) as f32 / frames_count;
+                                *left += packet[idx + 1].0 * left_scale * multiplier;
+                                *right += packet[idx + 1].1 * right_scale * multiplier;
+
+                                // The last iteration will leave "left" and "right" immutated. So
+                                // the last frame will be (0., 0.) if !got_samples.
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        // if no samples were played, reset the multiplier
-        if should_restart_exponential {
-            self.multiplier = EXPONENIAL_MULTIPLIER;
-        }
-
-        self.last_sample_value = *frames.last().unwrap();
+        self.last_frame = *out_frames.last().unwrap();
 
         DataCallbackResult::Continue
     }
@@ -180,11 +211,13 @@ pub async fn play_audio_loop(
 ) -> StrResult {
     let (_shutdown_notifier, shutdown_receiver) = smpsc::channel::<BytesMut>();
 
-    let sample_buffer = Arc::new(Mutex::new(VecDeque::new()));
-    let last_packet_samples_count = Arc::new(AtomicUsize::new(0));
+    let frame_buffer = Arc::new(Mutex::new(VecDeque::new()));
+    let last_packet = Arc::new(Mutex::new(None));
+    let disrupted = Arc::new(AtomicBool::new(false));
     thread::spawn({
-        let sample_buffer = sample_buffer.clone();
-        let last_packet_samples_count = last_packet_samples_count.clone();
+        let frame_buffer = frame_buffer.clone();
+        let last_packet = last_packet.clone();
+        let disrupted = disrupted.clone();
         move || -> StrResult {
             let mut stream = trace_err!(AudioStreamBuilder::default()
                 .set_shared()
@@ -196,12 +229,10 @@ pub async fn play_audio_loop(
                 .set_output()
                 .set_usage(Usage::Game)
                 .set_callback(PlayerCallback {
-                    sample_buffer,
-                    buffer_range_multiplier: buffer_range_multiplier as _,
-                    last_packet_samples_count,
-                    multiplier: 1.,
-                    exponential_last_value: (0., 0.),
-                    last_sample_value: (0., 0.),
+                    frame_buffer,
+                    last_packet,
+                    last_frame: (0., 0.),
+                    disrupted,
                 })
                 .open_stream())?;
 
@@ -218,15 +249,30 @@ pub async fn play_audio_loop(
 
     loop {
         let (_, data) = receiver.recv_buffer().await?;
-        let samples_it = data.chunks_exact(4).map(|c| {
-            (
-                i16::from_ne_bytes([c[0], c[1]]).to_f32(),
-                i16::from_ne_bytes([c[2], c[3]]).to_f32(),
-            )
-        });
+        let frames = data
+            .chunks_exact(4)
+            .map(|c| {
+                (
+                    i16::from_ne_bytes([c[0], c[1]]).to_f32(),
+                    i16::from_ne_bytes([c[2], c[3]]).to_f32(),
+                )
+            })
+            .collect::<Vec<_>>();
 
-        last_packet_samples_count.store(samples_it.len(), Ordering::Relaxed);
+        let mut frame_buffer_ref = frame_buffer.lock();
 
-        sample_buffer.lock().extend(samples_it);
+        frame_buffer_ref.extend(&frames);
+        *last_packet.lock() = Some(frames);
+
+        // todo: use smarter policy with EventTiming
+        if frame_buffer_ref.len() > 2 * buffer_range_multiplier as usize * data.len() {
+            error!("draining audio buffer. size: {}", frame_buffer_ref.len());
+
+            let buffer_size = frame_buffer_ref.len();
+            frame_buffer_ref
+                .drain(0..(buffer_size - buffer_range_multiplier as usize * data.len()));
+
+            disrupted.store(true, Ordering::Relaxed);
+        }
     }
 }

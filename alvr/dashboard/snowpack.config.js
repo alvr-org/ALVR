f82dd9f7a3596acbdb2401641e0df4b3d2359508
zarik5 <riccardo.zaglia5@gmail.com>
2021-03-03 13:36:13 +0100

    Update dashboard lints

--- alvr/dashboard/snowpack.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {
-    mount: {
-        src: "/",
-        static: "/",
-    },
-    plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-typescript"],
-}

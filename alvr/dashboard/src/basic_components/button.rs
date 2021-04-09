97c6c123ab495324e7fb65ee37ed00bc2edd6114
zarik5 <riccardo.zaglia5@gmail.com>
2021-04-09 22:25:34 +0200

    Re-add Bootstrap and import old css

--- alvr/dashboard/src/basic_components/button.rs
+++ alvr/dashboard/src/basic_components/button.rs
@@ -8,15 +8,10 @@ pub struct Props {
     pub onclick: Callback<MouseEvent>,
 }
 
-// note: this is an example, buttons should not need a Yew component, tailwind-css classes should be grouped into custom
-// classes and the imported where needed. This way we don't need to forward children and events
 #[function_component(Button)]
 pub fn button(Props { children, onclick }: &Props) -> Html {
     html! {
-        <button
-            class="relative bg-blue-500 text-white p-6 rounded text-2xl font-bold overflow-visible"
-            onclick=onclick
-        >
+        <button class="btn btn-primary" onclick=onclick>
             {children.clone()}
         </button>
     }
--- alvr/dashboard/src/index.js
+++ alvr/dashboard/src/index.js
@@ -1,3 +1,6 @@
-import "tailwindcss/tailwind.css"
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/js/solid"
+import "../resources/style.css"
 
 import("../pkg/index.js").catch(console.error)

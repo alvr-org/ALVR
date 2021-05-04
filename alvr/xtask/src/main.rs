0d49896eebc2849c51a004c36f8f4dc4ac0f5b17
zarik5 <riccardo.zaglia5@gmail.com>
2021-05-04 18:18:34 +0200

    Fix typo

--- alvr/xtask/src/main.rs
+++ alvr/xtask/src/main.rs
@@ -145,7 +145,7 @@ fn build_vulkan_layer(is_release: bool) {
     fs::create_dir_all(&destination).unwrap();
 
     // generator + build
-    cmake::Config::new(workspace_dir().join("alvr").join("vulkan_layer"))
+    cmake::Config::new(workspace_dir().join("alvr").join("vulkan-layer"))
         .target("x86_64-unknown-linux-gnu")
         .host("x86_64-unknown-linux-gnu")
         .out_dir(&destination)

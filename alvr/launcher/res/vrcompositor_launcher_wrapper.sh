b42f9b434ee024094fc224fccb622fc08f2aa619
zarik5 <riccardo.zaglia5@gmail.com>
2021-05-04 17:25:21 +0200

    Update launcher script with new vulkan-layer build directory

--- alvr/launcher/res/vrcompositor_launcher_wrapper.sh
+++ alvr/launcher/res/vrcompositor_launcher_wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export VK_LAYER_PATH=$(cat $XDG_RUNTIME_DIR/alvr_dir.txt | rev | cut -d'/' -f3- | rev)/alvr/server/cpp/tools/vulkan-layer/build
+export VK_LAYER_PATH=$(cat $XDG_RUNTIME_DIR/alvr_dir.txt | rev | cut -d'/' -f3- | rev)/build/vulkan-layer
 export VK_INSTANCE_LAYERS=VK_LAYER_ALVR_capture
 export DISABLE_VK_LAYER_VALVE_steam_fossilize_1=1
 export DISABLE_VK_LAYER_VALVE_steam_overlay_1=1

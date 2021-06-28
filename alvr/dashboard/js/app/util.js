8150cd1e21fb28a68aa8efb8894e1b4658c02cdf
sctanf <36978460+sctanf@users.noreply.github.com>
2021-06-28 16:47:18 -0500

    Suggested changes

---
+++ alvr/dashboard/js/app/util.js
@@ -0,0 +1,11 @@
+const quantile = (arr, q) => {
+    const sorted = arr.sort((a, b) => a - b);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+};

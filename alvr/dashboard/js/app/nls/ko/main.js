565472cc1263dfbd360cf5f32a8645c1e2202ed6
GiftShower_ <58767497+GiftShower@users.noreply.github.com>
2021-10-18 21:35:37 +0900

    Update main.js

--- alvr/dashboard/js/app/nls/ko/main.js
+++ alvr/dashboard/js/app/nls/ko/main.js
@@ -9,7 +9,7 @@ define({
     "systemLanguage": "시스템 언어",
     // Settings page
     "toggleAdvanced": "고급 옵션 보기",
-    "settingsGuide": "설정 가이드",
+    "settingsGuide": "설정 안내",
     "restartSteamVR": "SteamVR 재시작",
     // Installation page
     "runSetupWizard": "설치 마법사 실행하기",
@@ -18,11 +18,11 @@ define({
     "firewallSuccess": "방화벽 설정 완료",
     // About page
     "aboutText": "Wi-Fi를 통해 PC에서 헤드셋으로 VR게임을 스트리밍하세요. <br/> ALVR은 <a target= '_blank' href='https://developer.oculus.com/documentation/native/android/mobile-timewarp-overview/'>Asynchronous TimeWarp (ATW)</a> 과 <a target= '_blank' href='https://developer.oculus.com/documentation/native/android/mobile-ffr/'>Fixed Foveated Rendering (FFR)</a>기술을 이용하여 부드러운 경험을 제공합니다. <br/> 오큘러스 리프트(S)와 호환되는 게임은 ALVR에서 작동됩니다. <br/> 오큘러스 퀘스트와 고에서 작동되는 ALVR의 fork입니다. <br/> <br/> GitHub 방문하기: <a target= '_blank' href='https://github.com/alvr-org/ALVR/'>https://github.com/alvr-org/ALVR/</a> <br/> Discord 방문하기: <a target='_blank' href='https://discord.gg/ALVR'>https://discord.gg/ALVR</a>",
-    "checkForUpdates": "업데이트 확인",
+    "checkForUpdates": "업데이트 확인하기",
     "noNeedForUpdate": "최신버전 입니다.",
-    "needUpdateClickForMore": "업데이트 가능합니다, 클릭하여 확인하세요.",
+    "needUpdateClickForMore": "업데이트가 가능합니다. 클릭하여 확인하세요.",
     "updatePopupTitle": "업데이트가 있습니다!",
     "cancelUpdateButton": "무시하기",
     "moreUpdateButton": "웹사이트",
-    "okUpdateButton": "업데이트",
+    "okUpdateButton": "설치하기",
 });

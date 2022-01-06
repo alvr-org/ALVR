1bb363ed18a6c10849c76c297efb4151948a1181
Anton Palgunov <toxblh@gmail.com>
2022-01-06 00:49:16 +0000

    cargo fmt

--- alvr/xtask/src/packaging.rs
+++ alvr/xtask/src/packaging.rs
@@ -105,7 +105,7 @@ fn build_windows_installer(wix_path: &str) {
 pub fn publish_server(is_nightly: bool, root: Option<String>, reproducible: bool) {
     let bundle_ffmpeg = cfg!(target_os = "linux");
     build_server(true, false, false, bundle_ffmpeg, false, root, reproducible);
-    
+
     // Add licenses
     let licenses_dir = afs::server_build_dir().join("licenses");
     fs::create_dir_all(&licenses_dir).unwrap();

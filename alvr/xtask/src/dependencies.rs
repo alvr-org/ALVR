de106ab5b9e95458def6cfb2cec6c561010dad03
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-01-30 17:05:53 +0100

    Update Android dependencies

--- alvr/xtask/src/dependencies.rs
+++ alvr/xtask/src/dependencies.rs
@@ -16,60 +16,6 @@ fn download_and_extract_zip(url: &str, destination: &Path) {
     fs::remove_file(zip_file).unwrap();
 }
 
-fn build_rust_android_gradle() {
-    const PLUGIN_COMMIT: &str = "6e553c13ef2d9bb40b58a7675b96e0757d1b0443";
-    const PLUGIN_VERSION: &str = "0.8.3";
-
-    // Note: build_dir is used because the task always fails to delete the temp_gradle_plugin_build
-    // at the end. CI will try to zip the deps dir and this folder must not be present
-    let temp_build_dir = afs::build_dir().join("temp_gradle_plugin_build");
-    download_and_extract_zip(
-        &format!(
-            "https://codeload.github.com/mozilla/rust-android-gradle/zip/{}",
-            PLUGIN_COMMIT
-        ),
-        &temp_build_dir,
-    );
-    let download_path = temp_build_dir.join(format!("rust-android-gradle-{}", PLUGIN_COMMIT));
-
-    #[cfg(windows)]
-    let gradlew_path = download_path.join("gradlew.bat");
-    #[cfg(not(windows))]
-    let gradlew_path = download_path.join("gradlew");
-
-    command::run_in(
-        &download_path,
-        &format!("{} publish", gradlew_path.to_string_lossy()),
-    )
-    .unwrap();
-
-    let dep_dir = afs::deps_dir().join("rust-android-gradle");
-    if let Err(e) = fs::create_dir_all(&dep_dir) {
-        if e.kind() != ErrorKind::AlreadyExists {
-            panic::panic_any(e);
-        }
-    }
-
-    // Workaround for long path issue on Windows - canonicalize
-    let plugin_path = download_path.canonicalize().unwrap();
-    let plugin_path = plugin_path
-        .join("samples")
-        .join("maven-repo")
-        .join("org")
-        .join("mozilla")
-        .join("rust-android-gradle")
-        .join("rust-android")
-        .join(PLUGIN_VERSION)
-        .join(format!("rust-android-{}.jar", PLUGIN_VERSION));
-    fs::copy(
-        plugin_path,
-        dep_dir.join(format!("rust-android-{}.jar", PLUGIN_VERSION)),
-    )
-    .unwrap();
-
-    fs::remove_dir_all(temp_build_dir).ok();
-}
-
 pub fn build_ffmpeg_linux(nvenc_flag: bool) -> std::path::PathBuf {
     /* dependencies: build-essential pkg-config nasm libva-dev libdrm-dev libvulkan-dev
                      libx264-dev libx265-dev libffmpeg-nvenc-dev nvidia-cuda-toolkit
@@ -163,7 +109,6 @@ pub fn build_deps(target_os: &str) {
         command::run("rustup target add aarch64-linux-android").unwrap();
         command::run("cargo install cargo-apk").unwrap();
 
-        build_rust_android_gradle();
         get_oculus_openxr_mobile_loader();
     } else {
         println!("Nothing to do for {}!", target_os)

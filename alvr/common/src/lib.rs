9876dcffc6a7fbaba7f0801343aa9f470f31253d
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-04-14 15:23:50 +0200

    Introduce protocol ID (unused)

--- alvr/common/src/lib.rs
+++ alvr/common/src/lib.rs
@@ -1,11 +1,17 @@
 mod logging;
 
 use once_cell::sync::Lazy;
-use semver::{Prerelease, Version};
+use semver::Version;
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
+    sync::atomic::{AtomicBool, Ordering},
 };
+
+pub mod prelude {
+    pub use crate::{enone, err, err_dbg, fmt_e, logging::*, StrResult};
+    pub use log::{debug, error, info, warn};
+}
 
 pub use glam;
 pub use log;
@@ -17,28 +23,14 @@ pub use semver;
 pub type StrResult<T = ()> = Result<T, String>;
 
 pub const ALVR_NAME: &str = "ALVR";
-
-pub mod prelude {
-    pub use crate::{enone, err, err_dbg, fmt_e, logging::*, StrResult};
-    pub use log::{debug, error, info, warn};
-}
-
 pub static ALVR_VERSION: Lazy<Version> =
     Lazy::new(|| Version::parse(env!("CARGO_PKG_VERSION")).unwrap());
 
-// accept semver-compatible versions
-// Note: by not having to set the requirement manually, the major version is constrained to be
-// bumped when the packet layouts or some critical behaviour has changed.
-pub fn is_version_compatible(other_version: &Version) -> bool {
-    if other_version.pre != Prerelease::EMPTY || ALVR_VERSION.pre != Prerelease::EMPTY {
-        other_version.major == ALVR_VERSION.major
-            && other_version.minor == ALVR_VERSION.minor
-            && other_version.patch == ALVR_VERSION.patch
-            && other_version.pre == ALVR_VERSION.pre
-        // Note: metadata (+) is always ignored in the version check
-    } else {
-        other_version.major == ALVR_VERSION.major
-    }
+// Consistent across architectures, might not be consistent across different compiler versions.
+pub fn hash_string(string: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    string.hash(&mut hasher);
+    hasher.finish()
 }
 
 pub fn is_nightly() -> bool {
@@ -46,14 +38,31 @@ pub fn is_nightly() -> bool {
 }
 
 pub fn is_stable() -> bool {
-    ALVR_VERSION.pre == Prerelease::EMPTY && !is_nightly()
+    ALVR_VERSION.pre.is_empty() && !is_nightly()
 }
 
-// Consistent across architectures, might not be consistent across different compiler versions.
-pub fn hash_string(string: &str) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    string.hash(&mut hasher);
-    hasher.finish()
+// Semver compatible versions will produce the same protocol ID. Protocol IDs are not ordered
+// As a convention, encode/decode the protocol ID bytes as little endian.
+// Only makor and
+pub fn protocol_id() -> u64 {
+    let protocol_string = if ALVR_VERSION.pre.is_empty() {
+        ALVR_VERSION.major.to_string()
+    } else {
+        format!("{}-{}", ALVR_VERSION.major, ALVR_VERSION.pre)
+    };
+
+    hash_string(&protocol_string)
+}
+
+// deprecated
+pub fn is_version_compatible(other_version: &Version) -> bool {
+    let protocol_string = if other_version.pre.is_empty() {
+        other_version.major.to_string()
+    } else {
+        format!("{}-{}", other_version.major, other_version.pre)
+    };
+
+    protocol_id() == hash_string(&protocol_string)
 }
 
 pub const HEAD_PATH: &str = "/user/head";
@@ -69,3 +78,21 @@ pub static LEFT_CONTROLLER_HAPTIC_ID: Lazy<u64> =
     Lazy::new(|| hash_string(LEFT_CONTROLLER_HAPTIC_PATH));
 pub static RIGHT_CONTROLLER_HAPTIC_ID: Lazy<u64> =
     Lazy::new(|| hash_string(RIGHT_CONTROLLER_HAPTIC_PATH));
+
+// Simple wrapper for AtomicBool when using Ordering::Relaxed. Deref cannot be implemented (cannot
+// return local reference)
+pub struct RelaxedAtomic(AtomicBool);
+
+impl RelaxedAtomic {
+    pub fn new(initial_value: bool) -> Self {
+        Self(AtomicBool::new(initial_value))
+    }
+
+    pub fn value(&self) -> bool {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    pub fn set(&self, value: bool) {
+        self.0.store(value, Ordering::Relaxed);
+    }
+}

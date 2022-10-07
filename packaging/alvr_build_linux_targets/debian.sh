33f38810716eac7f791dc4f9942d331141251866
Dagg <daggintosh@outlook.com>
2022-10-07 04:45:08 -0700

    Fix if statement

--- packaging/alvr_build_linux_targets/debian.sh
+++ packaging/alvr_build_linux_targets/debian.sh
@@ -71,13 +71,13 @@ build_debian_server() {
 
     cd "${repoDir}" > /dev/null || return 4
     log info 'Building ALVR server ...'
+    if [ -n ${kwArgs['--no-nvidia']} ]; then
+        cargo xtask prepare-deps --platform linux --gpl
+    else
+        cargo xtask prepare-deps --platform linux --gpl --no-nvidia
+    fi
     # Cargo does NOT like quotes
     # shellcheck disable=SC2086
-    if kwArgs['--no-nvidia']; then
-        cargo xtask prepare-deps --platform linux --gpl --no-nvidia
-    else
-        cargo xtask prepare-deps --platform linux --gpl
-    fi
     if cargo xtask build-server ${kwArgs['--server-args']:---release --gpl}; then
         cd - > /dev/null || return 4
     else

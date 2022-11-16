878cd8686ab001d7e63d810b17553bf4d1c000e1
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-11-16 15:19:32 +0800

    Show IP address in lobby room

--- alvr/client_core/src/platform/mod.rs
+++ alvr/client_core/src/platform/mod.rs
@@ -3,11 +3,18 @@ pub mod android;
 
 #[cfg(target_os = "android")]
 pub use android::{
-    context, device_name, try_get_microphone_permission, video_decoder_split, vm, DequeuedFrame,
-    VideoDecoderDequeuer, VideoDecoderEnqueuer,
+    context, device_model, local_ip, try_get_microphone_permission, video_decoder_split, vm,
+    DequeuedFrame, VideoDecoderDequeuer, VideoDecoderEnqueuer,
 };
 
 #[cfg(not(target_os = "android"))]
-pub fn device_name() -> String {
+pub fn device_model() -> String {
     "Wired headset".into()
+}
+
+#[cfg(not(target_os = "android"))]
+pub fn local_ip() -> std::net::IpAddr {
+    use std::net::{IpAddr, Ipv4Addr};
+
+    local_ip_address::local_ip().unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED))
 }

f2a4972f25f5ff1eeaa954d0124641822ee553b6
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-11-17 18:40:06 +0800

    Log to file with json format
    Make the egui dashboard use server log timestamps

--- alvr/server/src/lib.rs
+++ alvr/server/src/lib.rs
@@ -172,8 +172,13 @@ pub fn notify_application_update() {
 
 fn init() {
     let (log_sender, _) = broadcast::channel(web_server::WS_BROADCAST_CAPACITY);
+    let (legacy_events_sender, _) = broadcast::channel(web_server::WS_BROADCAST_CAPACITY);
     let (events_sender, _) = broadcast::channel(web_server::WS_BROADCAST_CAPACITY);
-    logging_backend::init_logging(log_sender.clone(), events_sender.clone());
+    logging_backend::init_logging(
+        log_sender.clone(),
+        legacy_events_sender.clone(),
+        events_sender.clone(),
+    );
 
     if let Some(runtime) = WEBSERVER_RUNTIME.lock().as_mut() {
         // Acquire and drop the data manager lock to create session.json if not present
@@ -195,6 +200,7 @@ fn init() {
 
         runtime.spawn(alvr_common::show_err_async(web_server::web_server(
             log_sender,
+            legacy_events_sender,
             events_sender,
         )));
 

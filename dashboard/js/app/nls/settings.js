252016cccad94e601954d35a4f376a1f62bd1942
Vixea <vixea0@gmail.com>
2022-11-22 20:09:48 -0600

    Update info on support of 10 bit encoding

--- dashboard/js/app/nls/settings.js
+++ dashboard/js/app/nls/settings.js
@@ -76,7 +76,7 @@ define({
             "Request realtime decoder priority (client)", // adv
         "_root_video_use10bitEncoder.name": "Reduce color banding",
         "_root_video_use10bitEncoder.description":
-            "This increases visual quality by streaming 10 bits per color channel instead of 8. Works on Windows with newer nVidia and AMD cards and software encoding.",
+            "This increases visual quality by streaming 10 bits per color channel instead of 8. Works on all supported platforms except nvidia on linux.",
         "_root_video_advancedCodecOptions_amfControls_usePreproc.name":
             "Enable Pre-Processor component",
         "_root_video_advancedCodecOptions_amfControls_usePreproc.description":

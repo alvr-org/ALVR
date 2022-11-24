alvr-org/ALVR

commit 25a494ecc6a9f1133afbaf52699602c00f8e343d

VAAPI: Add support for 10bit encoding (#1267)

alvr/server/cpp/platform/linux/EncodePipelineVAAPI.cpp
@@ -39,7 +39,7 @@ void set_hwframe_ctx(AVCodecContext *ctx, AVBufferRef *hw_device_ctx)
   }
   frames_ctx = (AVHWFramesContext *)(hw_frames_ref->data);
   frames_ctx->format = AV_PIX_FMT_VAAPI;
-  frames_ctx->sw_format = AV_PIX_FMT_NV12;
+  frames_ctx->sw_format = Settings::Instance().m_use10bitEncoder ? AV_PIX_FMT_P010 : AV_PIX_FMT_NV12;
   frames_ctx->width = ctx->width;
   frames_ctx->height = ctx->height;
   frames_ctx->initial_pool_size = 3;
@@ -128,7 +128,7 @@ alvr::EncodePipelineVAAPI::EncodePipelineVAAPI(VkFrame &input_frame, VkFrameCtx&
       AVUTIL.av_opt_set(encoder_ctx, "rc_mode", "2", 0); //CBR
       break;
     case ALVR_CODEC_H265:
-      encoder_ctx->profile = FF_PROFILE_HEVC_MAIN;
+      encoder_ctx->profile = Settings::Instance().m_use10bitEncoder ? FF_PROFILE_HEVC_MAIN_10 : FF_PROFILE_HEVC_MAIN;
       AVUTIL.av_opt_set(encoder_ctx, "rc_mode", "2", 0);
       break;
   }
@@ -184,7 +184,13 @@ alvr::EncodePipelineVAAPI::EncodePipelineVAAPI(VkFrame &input_frame, VkFrameCtx&
   inputs->pad_idx = 0;
   inputs->next = NULL;
 
-  if ((err = AVFILTER.avfilter_graph_parse_ptr(filter_graph, "scale_vaapi=format=nv12", &inputs, &outputs, NULL)) < 0)
+  std::string filters = "scale_vaapi=format=";
+  if (Settings::Instance().m_use10bitEncoder) {
+    filters += "p010";
+  } else {
+    filters += "nv12";
+  }
+  if ((err = AVFILTER.avfilter_graph_parse_ptr(filter_graph, filters.c_str(), &inputs, &outputs, NULL)) < 0)
   {
     throw alvr::AvException("avfilter_graph_parse_ptr failed:", err);
   }

commit 55161091d33b53831ddeaad0d9bdc232ae7c97ac

VAAPI: Fix h264 with 10bit encoding enabled
Regression from 25a494ecc6

alvr/server/cpp/platform/linux/EncodePipelineVAAPI.cpp
@@ -39,7 +39,7 @@ void set_hwframe_ctx(AVCodecContext *ctx, AVBufferRef *hw_device_ctx)
   }
   frames_ctx = (AVHWFramesContext *)(hw_frames_ref->data);
   frames_ctx->format = AV_PIX_FMT_VAAPI;
-  frames_ctx->sw_format = Settings::Instance().m_use10bitEncoder ? AV_PIX_FMT_P010 : AV_PIX_FMT_NV12;
+  frames_ctx->sw_format = Settings::Instance().m_codec == ALVR_CODEC_H265 && Settings::Instance().m_use10bitEncoder ? AV_PIX_FMT_P010 : AV_PIX_FMT_NV12;
   frames_ctx->width = ctx->width;
   frames_ctx->height = ctx->height;
   frames_ctx->initial_pool_size = 3;
@@ -185,7 +185,7 @@ alvr::EncodePipelineVAAPI::EncodePipelineVAAPI(VkFrame &input_frame, VkFrameCtx&
   inputs->next = NULL;
 
   std::string filters = "scale_vaapi=format=";
-  if (Settings::Instance().m_use10bitEncoder) {
+  if (Settings::Instance().m_codec == ALVR_CODEC_H265 && Settings::Instance().m_use10bitEncoder) {
     filters += "p010";
   } else {
     filters += "nv12";
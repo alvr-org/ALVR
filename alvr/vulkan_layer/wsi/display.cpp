e5c2b216ff838b8c347a95128c602b047c7cf3ca
David Rosca <nowrep@gmail.com>
2022-12-04 17:35:49 +0100

    vulkan_layer: Rewrite vsync fence to wait on conditional
    Throw away the queue hacks and implement it correctly
    by waiting on conditional variable.
    This also fixes validation errors that would complain
    about using VkFence from multiple threads.

--- alvr/vulkan_layer/wsi/display.cpp
+++ alvr/vulkan_layer/wsi/display.cpp
@@ -1,14 +1,10 @@
 #include "display.hpp"
-#include "layer/private_data.hpp"
 
 #include"layer/settings.h"
 
 #include <chrono>
 
-wsi::display::display(layer::device_private_data& device_data, uint32_t queue_family_index, uint32_t queue_index):
-  m_queue_family_index(queue_family_index),
-  m_queue_index(queue_index),
-  m_device_data(device_data)
+wsi::display::display()
 {
 }
 
@@ -16,34 +12,37 @@ VkFence wsi::display::get_vsync_fence()
 {
   if (not std::atomic_exchange(&m_thread_running, true))
   {
-  VkQueue queue;
-  VkFenceCreateInfo fence_info = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, nullptr, 0};
-  m_device_data.disp.CreateFence(m_device_data.device, &fence_info, nullptr, &vsync_fence);
-  m_device_data.disp.GetDeviceQueue(m_device_data.device, m_queue_family_index, m_queue_index, &queue);
-  m_device_data.SetDeviceLoaderData(m_device_data.device, queue);
-  m_vsync_thread = std::thread([this, queue]()
+  vsync_fence = reinterpret_cast<VkFence>(this);
+  m_vsync_thread = std::thread([this]()
       {
       auto refresh = Settings::Instance().m_refreshRate;
       auto next_frame = std::chrono::steady_clock::now();
       auto frame_time = std::chrono::duration_cast<decltype(next_frame)::duration>(std::chrono::duration<double>(1. / refresh));
       while (not m_exiting) {
-        if (m_device_data.disp.GetFenceStatus(m_device_data.device, vsync_fence) == VK_NOT_READY)
-        {
-          m_device_data.disp.QueueSubmit(queue, 0, nullptr, vsync_fence);
-        }
-        m_device_data.disp.QueueWaitIdle(queue);
         std::this_thread::sleep_until(next_frame);
+        m_signaled = true;
+        m_cond.notify_all();
         m_vsync_count += 1;
         next_frame += frame_time;
       }
-      m_device_data.disp.DestroyFence(m_device_data.device, vsync_fence, nullptr);
       });
   }
+  m_signaled = false;
   return vsync_fence;
 }
 
 wsi::display::~display() {
+  std::unique_lock<std::mutex> lock(m_mutex);
   m_exiting = true;
   if (m_vsync_thread.joinable())
     m_vsync_thread.join();
+}
+
+bool wsi::display::wait_for_vsync(uint64_t timeoutNs)
+{
+  if (!m_signaled) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    return m_cond.wait_for(lock, std::chrono::nanoseconds(timeoutNs)) == std::cv_status::no_timeout;
+  }
+  return true;
 }

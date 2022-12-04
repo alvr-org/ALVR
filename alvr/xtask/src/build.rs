a6c2c385b25ccf94c51b5d20a7cede1629bfef2b
Łukasz Mariański <lmarianski@protonmail.com>
2022-12-04 16:28:07 +0100

    rm launcher build, change main exec to dashboard

--- alvr/xtask/src/build.rs
+++ alvr/xtask/src/build.rs
@@ -101,18 +101,6 @@ pub fn build_server(
         }
     }
 
-    // build launcher
-    {
-        let _push_guard = sh.push_dir(afs::crate_dir("launcher"));
-        cmd!(sh, "cargo build {common_flags_ref...}").run().unwrap();
-
-        sh.copy_file(
-            artifacts_dir.join(afs::exec_fname("alvr_launcher")),
-            build_layout.launcher_exe(),
-        )
-        .unwrap();
-    }
-
     // Build dashboard
     {
         let _push_guard = sh.push_dir(afs::crate_dir("dashboard"));

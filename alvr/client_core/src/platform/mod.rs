b7b1c7a52b8ca6f21962ded87032c1bcc827117f
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-12-07 12:42:16 +0100

    Fix "cargo build" on macOS

--- alvr/client_core/src/platform/mod.rs
+++ alvr/client_core/src/platform/mod.rs
@@ -13,9 +13,14 @@ pub fn device_model() -> String {
     "Wired headset".into()
 }
 
-#[cfg(not(target_os = "android"))]
+#[cfg(not(any(target_os = "android", target_os = "macos")))]
 pub fn local_ip() -> std::net::IpAddr {
     use std::net::{IpAddr, Ipv4Addr};
 
     local_ip_address::local_ip().unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED))
+}
+
+#[cfg(target_os = "macos")]
+pub fn local_ip() -> std::net::IpAddr {
+    std::net::IpAddr::V4(std::net::Ipv4Addr::UNSPECIFIED)
 }

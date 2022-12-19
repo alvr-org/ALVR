c638889fea6902922ac68ea423c245a81c7142ce
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2022-12-19 18:04:17 +0100

    Try fix StreamVR standby

--- alvr/server/cpp/alvr_server/OvrHMD.cpp
+++ alvr/server/cpp/alvr_server/OvrHMD.cpp
@@ -191,6 +191,12 @@ vr::EVRInitError OvrHmd::Activate(vr::TrackedDeviceIndex_t unObjectId) {
     vr_properties->SetBoolProperty(
         this->prop_container, vr::Prop_DeviceProvidesBatteryStatus_Bool, true);
 
+    // Use dummy proximity sensor
+    vr::VRProperties()->SetBoolProperty(
+        this->prop_container, vr::Prop_ContainsProximitySensor_Bool, true);
+    vr::VRDriverInput()->CreateBooleanComponent(this->prop_container, "/proximity", &m_proximity);
+    vr::VRDriverInput()->UpdateBooleanComponent(m_proximity, true, 0.0);
+
 #ifdef _WIN32
     float originalIPD =
         vr::VRSettings()->GetFloat(vr::k_pch_SteamVR_Section, vr::k_pch_SteamVR_IPD_Float);

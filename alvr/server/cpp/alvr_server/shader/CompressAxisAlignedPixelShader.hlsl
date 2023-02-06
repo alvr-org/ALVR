53ac2e56cbf2f47449f4cc476a89628d13f0fca4
Yury <rip95_95@mail.ru>
2023-02-06 17:31:08 +0100

    Format FFR shaders and combine some variables

--- alvr/server/cpp/alvr_server/shader/CompressAxisAlignedPixelShader.hlsl
+++ alvr/server/cpp/alvr_server/shader/CompressAxisAlignedPixelShader.hlsl
@@ -12,34 +12,31 @@ SamplerState trilinearSampler {
 	//AddressV = Wrap;
 };
 
-float4 main(float2 uv : TEXCOORD0) : SV_Target{
+float4 main(float2 uv : TEXCOORD0) : SV_Target {
 	bool isRightEye = uv.x > 0.5;
-	float2 eyeUV = TextureToEyeUV(uv, isRightEye);
+	float2 eyeUV = TextureToEyeUV(uv, isRightEye) / eyeSizeRatio;
 
-	float2 alignedUV = eyeUV / eyeSizeRatio;
+	float2 c0 = (1. - centerSize) / 2.;
+	float2 c1 = (edgeRatio - 1.) * c0 * (centerShift + 1.) / edgeRatio;
+	float2 c2 = (edgeRatio - 1.) * centerSize + 1.;
 
-	float2 c0 = (1.-centerSize)/2.;
-	float2 c1 = (edgeRatio-1.)*c0*(centerShift+1.)/edgeRatio;
-	float2 c2 = (edgeRatio-1.)*centerSize+1.;
+	float2 loBound = c0 * (centerShift + 1.) / c2;
+	float2 hiBound = c0 * (centerShift - 1.) / c2 + 1.;
+	float2 underBound = float2(eyeUV.x < loBound.x, eyeUV.y < loBound.y);
+	float2 inBound = float2(loBound.x < eyeUV.x && eyeUV.x < hiBound.x,
+							loBound.y < eyeUV.y && eyeUV.y < hiBound.y);
+	float2 overBound = float2(eyeUV.x > hiBound.x, eyeUV.y > hiBound.y);
 
-	float2 loBound = c0*(centerShift+1.)/c2;
-	float2 hiBound = c0*(centerShift-1.)/c2+1.;
-	float2 underBound = float2(alignedUV.x<loBound.x,alignedUV.y<loBound.y);
-	float2 inBound = float2(loBound.x<alignedUV.x&&alignedUV.x<hiBound.x,loBound.y<alignedUV.y&&alignedUV.y<hiBound.y);
-	float2 overBound = float2(alignedUV.x>hiBound.x,alignedUV.y>hiBound.y);
+	float2 center = eyeUV * c2 / edgeRatio + c1;
+	float2 d2 = eyeUV * c2;
+	float2 d3 = (eyeUV - 1.) * c2 + 1.;
+	float2 g1 = eyeUV / loBound;
+	float2 g2 = (1. - eyeUV) / (1. - hiBound);
 
-	float2 d1 = alignedUV*c2/edgeRatio+c1;
-	float2 d2 = alignedUV*c2;
-	float2 d3 = (alignedUV-1.)*c2+1.;
-	float2 g1 = alignedUV/loBound;
-	float2 g2 = (1.-alignedUV)/(1.-hiBound);
+	float2 leftEdge = g1 * center + (1. - g1) * d2;
+	float2 rightEdge = g2 * center + (1. - g2) * d3;
 
-	float2 center = d1;
-	float2 leftEdge = g1*d1+(1.-g1)*d2;
-	float2 rightEdge = g2*d1+(1.-g2)*d3;
+	float2 compressedUV = underBound * leftEdge + inBound * center + overBound * rightEdge;
 
-	float2 compressedUV = underBound*leftEdge+inBound*center+overBound*rightEdge;
-
-	return compositionTexture.Sample(
-		trilinearSampler, EyeToTextureUV(compressedUV, isRightEye));
+	return compositionTexture.Sample(trilinearSampler, EyeToTextureUV(compressedUV, isRightEye));
 }

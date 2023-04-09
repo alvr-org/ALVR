279c690a86a8448d678cc8ba9263e8cb3f4b6296
David Rosca <nowrep@gmail.com>
2023-04-09 22:45:21 +0200

    EncodePipelineSW: Add x265 support

--- alvr/server/cpp/platform/linux/EncodePipelineSW.cpp
+++ alvr/server/cpp/platform/linux/EncodePipelineSW.cpp
@@ -1,6 +1,7 @@
 #include "EncodePipelineSW.h"
 
 #include <chrono>
+#include <string.h>
 
 #include "alvr_server/Settings.h"
 #include "alvr_server/Logger.h"
@@ -36,26 +37,39 @@ void x264_log(void *, int level, const char *fmt, va_list args)
 alvr::EncodePipelineSW::EncodePipelineSW(Renderer *render, uint32_t width, uint32_t height)
 {
   const auto& settings = Settings::Instance();
+  use_x264 = settings.m_codec == ALVR_CODEC_H264;
 
-  if (settings.m_codec == ALVR_CODEC_H265)
-  {
-    // TODO: Make it work?
-    throw std::runtime_error("HEVC is not supported by SW encoder");
+  if (use_x264) {
+    x264_param_default_preset(&avc.param, "ultrafast", "zerolatency");
+    x264_param_apply_profile(&avc.param, "high");
+
+    avc.param.pf_log = x264_log;
+    avc.param.i_log_level = X264_LOG_INFO;
+
+    avc.param.b_aud = 0;
+    avc.param.b_cabac = settings.m_entropyCoding == ALVR_CABAC;
+    avc.param.b_sliced_threads = true;
+    avc.param.i_threads = settings.m_swThreadCount;
+    avc.param.i_width = width;
+    avc.param.i_height = height;
+    avc.param.rc.i_rc_method = X264_RC_ABR;
+  } else {
+    hevc.api = x265_api_get(8);
+    if (!hevc.api) {
+      throw std::runtime_error("Failed to get x265 api");
+    }
+    hevc.param = hevc.api->param_alloc();
+    hevc.api->param_default_preset(hevc.param, "ultrafast", "zerolatency");
+    hevc.api->param_apply_profile(hevc.param, "main");
+
+    hevc.param->logLevel = X265_LOG_INFO;
+
+    hevc.param->sourceWidth = width;
+    hevc.param->sourceHeight = height;
+    hevc.param->rc.rateControlMode = X265_RC_ABR;
+    hevc.param->internalCsp = X265_CSP_I420;
+    hevc.param->bRepeatHeaders = 1;
   }
-
-  x264_param_default_preset(&param, "ultrafast", "zerolatency");
-  x264_param_apply_profile(&param, "high");
-
-  param.pf_log = x264_log;
-  param.i_log_level = X264_LOG_INFO;
-
-  param.b_aud = 0;
-  param.b_cabac = settings.m_entropyCoding == ALVR_CABAC;
-  param.b_sliced_threads = true;
-  param.i_threads = settings.m_swThreadCount;
-  param.i_width = width;
-  param.i_height = height;
-  param.rc.i_rc_method = X264_RC_ABR;
 
   auto params = FfiDynamicEncoderParams {};
   params.updated = true;
@@ -63,16 +77,28 @@ alvr::EncodePipelineSW::EncodePipelineSW(Renderer *render, uint32_t width, uint3
   params.framerate = Settings::Instance().m_refreshRate;
   SetParams(params);
 
-  enc = x264_encoder_open(&param);
-  if (!enc) {
+  bool opened = false;
+  if (use_x264) {
+    avc.enc = x264_encoder_open(&avc.param);
+    opened = avc.enc;
+  } else {
+    hevc.enc = hevc.api->encoder_open(hevc.param);
+    opened = hevc.enc;
+  }
+  if (!opened) {
     throw std::runtime_error("Failed to open encoder");
   }
 
-  x264_picture_init(&picture);
-  picture.img.i_csp = X264_CSP_I420;
-  picture.img.i_plane = 3;
+  if (use_x264) {
+    x264_picture_init(&avc.picture);
+    avc.picture.img.i_csp = X264_CSP_I420;
+    avc.picture.img.i_plane = 3;
 
-  x264_picture_init(&picture_out);
+    x264_picture_init(&avc.picture_out);
+  } else {
+    hevc.api->picture_init(hevc.param, &hevc.picture);
+    hevc.api->picture_init(hevc.param, &hevc.picture_out);
+  }
 
   rgbtoyuv = new RgbToYuv420(render, render->GetOutput().image, render->GetOutput().imageInfo, render->GetOutput().semaphore);
 }
@@ -82,34 +108,67 @@ alvr::EncodePipelineSW::~EncodePipelineSW()
   if (rgbtoyuv) {
     delete rgbtoyuv;
   }
-  if (enc) {
-    x264_encoder_close(enc);
+  if (use_x264) {
+    if (avc.enc) {
+      x264_encoder_close(avc.enc);
+    }
+  } else {
+    if (hevc.param) {
+      hevc.api->param_free(hevc.param);
+    }
+    if (hevc.enc) {
+      hevc.api->encoder_close(hevc.enc);
+    }
   }
 }
 
 void alvr::EncodePipelineSW::PushFrame(uint64_t targetTimestampNs, bool idr)
 {
-  rgbtoyuv->Convert(picture.img.plane, picture.img.i_stride);
+  if (use_x264) {
+    rgbtoyuv->Convert(avc.picture.img.plane, avc.picture.img.i_stride);
+    avc.picture.i_type = idr ? X264_TYPE_IDR : X264_TYPE_AUTO;
+    pts = avc.picture.i_pts = targetTimestampNs;
+  } else {
+    rgbtoyuv->Convert(reinterpret_cast<uint8_t**>(hevc.picture.planes), hevc.picture.stride);
+    hevc.picture.sliceType = idr ? X265_TYPE_IDR : X265_TYPE_AUTO;
+    pts = hevc.picture.pts = targetTimestampNs;
+  }
   rgbtoyuv->Sync();
   timestamp.cpu = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 
-  picture.i_type = idr ? X264_TYPE_IDR : X264_TYPE_AUTO;
-  pts = picture.i_pts = targetTimestampNs;
-
-  int nnal = 0;
-  nal_size = x264_encoder_encode(enc, &nal, &nnal, &picture, &picture_out);
-  if (nal_size < 0) {
-    throw std::runtime_error("x264 encoder_encode failed");
+  if (use_x264) {
+    int nnal = 0;
+    nal_size = x264_encoder_encode(avc.enc, &avc.nal, &nnal, &avc.picture, &avc.picture_out);
+    if (nal_size < 0) {
+      throw std::runtime_error("x264 encoder_encode failed");
+    }
+  } else {
+    uint32_t nnal = 0;
+    int ret = hevc.api->encoder_encode(hevc.enc, &hevc.nal, &nnal, &hevc.picture, &hevc.picture_out);
+    if (ret < 0) {
+      throw std::runtime_error("x265 encoder_encode failed");
+    }
+    nal_size = 0;
+    for (uint32_t i = 0; i < nnal; ++i) {
+      nal_size += hevc.nal[i].sizeBytes;
+    }
   }
 }
 
 bool alvr::EncodePipelineSW::GetEncoded(FramePacket &packet)
 {
-  if (!nal) {
-    return false;
+  if (use_x264) {
+    if (!avc.nal) {
+      return false;
+    }
+    packet.data = avc.nal[0].p_payload;
+  } else {
+    if (!hevc.nal) {
+      return false;
+    }
+    packet.data = hevc.nal[0].payload;
   }
   packet.size = nal_size;
-  packet.data = nal[0].p_payload;
   packet.pts = pts;
   return packet.size > 0;
 }
@@ -119,14 +178,27 @@ void alvr::EncodePipelineSW::SetParams(FfiDynamicEncoderParams params)
   if (!params.updated) {
     return;
   }
-  // x264 doesn't work well with adaptive bitrate/fps
-  param.i_fps_num = Settings::Instance().m_refreshRate;
-  param.i_fps_den = 1;
-  param.rc.i_bitrate = params.bitrate_bps / 1'000 * 1.4; // needs higher value to hit target bitrate
-  param.rc.i_vbv_buffer_size = param.rc.i_bitrate / param.i_fps_num * 1.1;
-  param.rc.i_vbv_max_bitrate = param.rc.i_bitrate;
-  param.rc.f_vbv_buffer_init = 0.75;
-  if (enc) {
-    x264_encoder_reconfig(enc, &param);
+  if (use_x264) {
+    // x264 doesn't work well with adaptive bitrate/fps
+    avc.param.i_fps_num = Settings::Instance().m_refreshRate;
+    avc.param.i_fps_den = 1;
+    avc.param.rc.i_bitrate = params.bitrate_bps / 1'000 * 1.4; // needs higher value to hit target bitrate
+    avc.param.rc.i_vbv_buffer_size = avc.param.rc.i_bitrate / avc.param.i_fps_num * 1.1;
+    avc.param.rc.i_vbv_max_bitrate = avc.param.rc.i_bitrate;
+    avc.param.rc.f_vbv_buffer_init = 0.75;
+    if (avc.enc) {
+      x264_encoder_reconfig(avc.enc, &avc.param);
+    }
+  } else {
+    // x265 doesn't work well with adaptive bitrate/fps
+    hevc.param->fpsNum = Settings::Instance().m_refreshRate;
+    hevc.param->fpsDenom = 1;
+    hevc.param->rc.bitrate = params.bitrate_bps / 1'000 * 1.4; // needs higher value to hit target bitrate
+    hevc.param->rc.vbvBufferSize = hevc.param->rc.bitrate / hevc.param->fpsNum * 1.1;
+    hevc.param->rc.vbvMaxBitrate = hevc.param->rc.bitrate;
+    hevc.param->rc.vbvBufferInit = 0.75;
+    if (hevc.enc) {
+      hevc.api->encoder_reconfig(hevc.enc, hevc.param);
+    }
   }
 }
--- alvr/server/cpp/platform/linux/EncodePipelineSW.h
+++ alvr/server/cpp/platform/linux/EncodePipelineSW.h
@@ -3,6 +3,7 @@
 #include "EncodePipeline.h"
 
 #include <x264.h>
+#include <x265.h>
 
 class FormatConverter;
 
@@ -20,11 +21,22 @@ public:
   void SetParams(FfiDynamicEncoderParams params) override;
 
 private:
-  x264_t *enc = nullptr;
-  x264_param_t param;
-  x264_picture_t picture;
-  x264_picture_t picture_out;
-  x264_nal_t *nal = nullptr;
+  struct {
+    x264_t *enc = nullptr;
+    x264_param_t param;
+    x264_picture_t picture;
+    x264_picture_t picture_out;
+    x264_nal_t *nal = nullptr;
+  } avc;
+  struct {
+    const x265_api *api = nullptr;
+    x265_encoder *enc = nullptr;
+    x265_param *param = nullptr;
+    x265_picture picture;
+    x265_picture picture_out;
+    x265_nal *nal = nullptr;
+  } hevc;
+  bool use_x264 = false;
   int nal_size = 0;
   int64_t pts = 0;
   FormatConverter *rgbtoyuv = nullptr;

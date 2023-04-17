ff20129198655805be8f51656494eb256ea553a1
David Rosca <nowrep@gmail.com>
2023-04-17 11:32:07 +0200

    Allow encoders to override codec to use (#1568)
    Used in software encoder where only h264 is available.

--- alvr/server/cpp/platform/win32/VideoEncoderNVENC.cpp
+++ alvr/server/cpp/platform/win32/VideoEncoderNVENC.cpp
@@ -118,7 +118,7 @@ void VideoEncoderNVENC::Transmit(ID3D11Texture2D *pTexture, uint64_t presentatio
 			fpOut.write(reinterpret_cast<char*>(packet.data()), packet.size());
 		}
 		
-		ParseFrameNals(packet.data(), (int)packet.size(), targetTimestampNs);
+		ParseFrameNals(m_codec, packet.data(), (int)packet.size(), targetTimestampNs);
 	}
 }
 

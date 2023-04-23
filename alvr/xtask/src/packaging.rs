f6dc1083d1565ec3cc3f93315f9a7b3c70ef2367
Harsha Raghu <narnindi.raghu@gmail.com>
2023-04-23 17:55:20 +0530

    Update packaging.rs

--- alvr/xtask/src/packaging.rs
+++ alvr/xtask/src/packaging.rs
@@ -85,8 +85,6 @@ pub fn package_streamer(gpl: bool, root: Option<String>, appimage: bool, zsync: 
     // Finally package everything
     if cfg!(windows) {
         command::zip(&sh, &afs::streamer_build_dir()).unwrap();
-
-        // the wix file broke
         build_windows_installer();
     } else {
         command::targz(&sh, &afs::streamer_build_dir()).unwrap();

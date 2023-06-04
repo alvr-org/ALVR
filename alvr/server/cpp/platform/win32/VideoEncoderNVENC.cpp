cadc2dcb459361b24d509b6556d0c58e546aebf4
arnoldtsui <tsuiarnold@gmail.com>
2023-06-04 19:04:18 +0800

    debugging

--- alvr/server/cpp/platform/win32/VideoEncoderNVENC.cpp
+++ alvr/server/cpp/platform/win32/VideoEncoderNVENC.cpp
@@ -266,11 +266,11 @@ void VideoEncoderNVENC::FillEncodeConfig(NV_ENC_INITIALIZE_PARAMS &initializePar
 	encodeConfig.rcParams.vbvInitialDelay = maxFrameSize * 1.1;
 	encodeConfig.rcParams.maxBitRate = static_cast<uint32_t>(bitrate_bps);
 	encodeConfig.rcParams.averageBitRate = static_cast<uint32_t>(bitrate_bps);
-	// if (Settings::Instance().m_nvencAdaptiveQuantizationMode == SpatialAQ) {
-	// 	encodeConfig.rcParams.enableAQ = 1;
-	// } else if (Settings::Instance().m_nvencAdaptiveQuantizationMode == TemporalAQ) {
-	// 	encodeConfig.rcParams.enableTemporalAQ = 1;
-	// }
+	if (Settings::Instance().m_nvencAdaptiveQuantizationMode == SpatialAQ) {
+		encodeConfig.rcParams.enableAQ = 1;
+	} else if (Settings::Instance().m_nvencAdaptiveQuantizationMode == TemporalAQ) {
+		encodeConfig.rcParams.enableTemporalAQ = 1;
+	}
 
 	if (Settings::Instance().m_nvencRateControlMode != -1) {
 		encodeConfig.rcParams.rateControlMode = (NV_ENC_PARAMS_RC_MODE)Settings::Instance().m_nvencRateControlMode;

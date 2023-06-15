4126e6a323c044acc21aa0452719ce8a68e045f9
Yury <rip95_95@mail.ru>
2023-06-15 21:08:31 +0300

    Fix depth texture creating in OvrDirectModeComponent::CreateSwapTextureSet

--- alvr/server/cpp/platform/win32/OvrDirectModeComponent.cpp
+++ alvr/server/cpp/platform/win32/OvrDirectModeComponent.cpp
@@ -20,19 +20,23 @@ void OvrDirectModeComponent::CreateSwapTextureSet(uint32_t unPid, const SwapText
 	//HRESULT hr = D3D11CreateDevice(pAdapter, D3D_DRIVER_TYPE_HARDWARE, NULL, creationFlags, NULL, 0, D3D11_SDK_VERSION, &pDevice, &eFeatureLevel, &pContext);
 
 	D3D11_TEXTURE2D_DESC SharedTextureDesc = {};
+	DXGI_FORMAT format = (DXGI_FORMAT)pSwapTextureSetDesc->nFormat;
+	SharedTextureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+	if (format == DXGI_FORMAT_R32G8X24_TYPELESS || format == DXGI_FORMAT_R32_TYPELESS) {
+		SharedTextureDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	}
 	SharedTextureDesc.ArraySize = 1;
 	SharedTextureDesc.MipLevels = 1;
 	SharedTextureDesc.SampleDesc.Count = pSwapTextureSetDesc->nSampleCount;
 	SharedTextureDesc.SampleDesc.Quality = 0;
 	SharedTextureDesc.Usage = D3D11_USAGE_DEFAULT;
-	SharedTextureDesc.Format = (DXGI_FORMAT)pSwapTextureSetDesc->nFormat;
+	SharedTextureDesc.Format = format;
 
 	// Some(or all?) applications request larger texture than we specified in GetRecommendedRenderTargetSize.
 	// But, we must create textures in requested size to prevent cropped output. And then we must shrink texture to H.264 movie size.
 	SharedTextureDesc.Width = pSwapTextureSetDesc->nWidth;
 	SharedTextureDesc.Height = pSwapTextureSetDesc->nHeight;
 
-	SharedTextureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 	//SharedTextureDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX | D3D11_RESOURCE_MISC_SHARED_NTHANDLE;
 	SharedTextureDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
 

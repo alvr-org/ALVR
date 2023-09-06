ad6d2089051a5584a50d518c7687f2e4d8dd799d
KillerMemz <yackar@aim.com>
2023-09-06 17:24:56 +0000

    Update dependencies.rs to ONLY apply patches before the nvenc check

--- alvr/xtask/src/dependencies.rs
+++ alvr/xtask/src/dependencies.rs
@@ -200,10 +200,6 @@ pub fn build_ffmpeg_linux(nvenc_flag: bool) {
             .unwrap();
     }
 
-    // Patches ffmpeg for workarounds and patches that have yet to be unstreamed
-    let ffmpeg_command = "for p in ../../../alvr/xtask/patches/*; do patch -p1 < $p; done";
-    cmd!(sh, "bash -c {ffmpeg_command}").run().unwrap();
-
     let nproc = cmd!(sh, "nproc").read().unwrap();
     cmd!(sh, "make -j{nproc}").run().unwrap();
     cmd!(sh, "make install").run().unwrap();

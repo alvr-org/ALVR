ee3d86072bcf4f3cbb53670dfed139462e9319e0
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2023-10-15 01:03:27 +0800

    feat(dashboard): :sparkles: Add notification tips

--- alvr/dashboard/src/dashboard/components/notifications.rs
+++ alvr/dashboard/src/dashboard/components/notifications.rs
@@ -6,6 +6,7 @@ use eframe::{
     emath::Align,
     epaint::{Color32, Stroke},
 };
+use rand::seq::SliceRandom;
 use std::time::Duration;
 
 #[cfg(target_arch = "wasm32")]
@@ -13,30 +14,46 @@ use instant::Instant;
 #[cfg(not(target_arch = "wasm32"))]
 use std::time::Instant;
 
-const NO_NOTIFICATIONS: &str = "No new notifications";
 const TIMEOUT: Duration = Duration::from_secs(5);
+const NO_NOTIFICATIONS_MESSAGE: &str = "No new notifications";
+const NOTIFICATION_TIPS: &[&str] = &[
+    r#"If you want to use body trackers, set "Driver launch action" to "Unregister ALVR at shutdown""#,
+    "If you're using NVIDIA gpu, best to use high bitrate H264, if you're using AMD gpu, HEVC might look better",
+];
 
 pub struct NotificationBar {
     message: String,
     current_level: LogSeverity,
     receive_instant: Instant,
     min_notification_level: LogSeverity,
+    tip_message: Option<String>,
     expanded: bool,
 }
 
 impl NotificationBar {
     pub fn new() -> Self {
         Self {
-            message: NO_NOTIFICATIONS.into(),
+            message: NO_NOTIFICATIONS_MESSAGE.into(),
             current_level: LogSeverity::Debug,
             receive_instant: Instant::now(),
             min_notification_level: LogSeverity::Debug,
+            tip_message: None,
             expanded: false,
         }
     }
 
     pub fn update_settings(&mut self, settings: &Settings) {
         self.min_notification_level = settings.logging.notification_level;
+
+        if settings.logging.show_notification_tip {
+            if self.tip_message.is_none() {
+                self.tip_message = NOTIFICATION_TIPS
+                    .choose(&mut rand::thread_rng())
+                    .map(|s| format!("Tip: {s}"));
+            }
+        } else {
+            self.tip_message = None;
+        }
     }
 
     pub fn push_notification(&mut self, event: LogEntry) {
@@ -53,7 +70,10 @@ impl NotificationBar {
     pub fn ui(&mut self, context: &egui::Context) {
         let now = Instant::now();
         if now > self.receive_instant + TIMEOUT {
-            self.message = NO_NOTIFICATIONS.into();
+            self.message = self
+                .tip_message
+                .clone()
+                .unwrap_or_else(|| NO_NOTIFICATIONS_MESSAGE.into());
             self.current_level = LogSeverity::Debug;
         }
 

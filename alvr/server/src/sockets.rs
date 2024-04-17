2245512a1770889f25906672eb5c2e8d37ab6ec6
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-04-18 03:24:42 +0800

    fix(server): :bug: Use device_id field for hostname instead of mDNS hostname

--- alvr/server/src/sockets.rs
+++ alvr/server/src/sockets.rs
@@ -87,22 +87,32 @@ impl WelcomeSocket {
             match self.mdns_receiver.try_recv() {
                 Ok(event) => {
                     if let ServiceEvent::ServiceResolved(info) = event {
-                        let hostname = info.get_hostname();
+                        let hostname = info
+                            .get_property_val_str(alvr_sockets::MDNS_DEVICE_ID_KEY)
+                            .unwrap_or_else(|| info.get_hostname());
                         let address = *info.get_addresses().iter().next().to_any()?;
 
-                        let protocol = info
+                        let client_protocol = info
                             .get_property_val_str(alvr_sockets::MDNS_PROTOCOL_KEY)
                             .to_any()?;
+                        let server_protocol = alvr_common::protocol_id();
+                        let client_is_dev = client_protocol.contains("-dev");
+                        let server_is_dev = server_protocol.contains("-dev");
 
-                        if protocol != alvr_common::protocol_id() {
-                            let msg = format!(
-                                r#"Expected protocol ID "{}", found "{}""#,
-                                alvr_common::protocol_id(),
-                                protocol
+                        if client_protocol != server_protocol {
+                            let reason = if client_is_dev && server_is_dev {
+                                "Please use matching nightly versions."
+                            } else if client_is_dev {
+                                "Please use nightly server or stable client."
+                            } else if server_is_dev {
+                                "Please use stable server or nightly client."
+                            } else {
+                                "Please use matching stable versions."
+                            };
+                            let protocols = format!(
+                                "Protocols: server={server_protocol}, client={client_protocol}"
                             );
-                            warn!(
-                                "Found incompatible client {hostname}! Upgrade or downgrade\n{msg}"
-                            );
+                            warn!("Found incompatible client {hostname}! {reason}\n{protocols}");
                         }
 
                         clients.insert(hostname.into(), address);
--- alvr/sockets/src/lib.rs
+++ alvr/sockets/src/lib.rs
@@ -21,6 +21,7 @@ pub const KEEPALIVE_TIMEOUT: Duration = Duration::from_secs(2);
 
 pub const MDNS_SERVICE_TYPE: &str = "_alvr._tcp.local.";
 pub const MDNS_PROTOCOL_KEY: &str = "protocol";
+pub const MDNS_DEVICE_ID_KEY: &str = "device_id";
 
 fn set_socket_buffers(
     socket: &socket2::Socket,

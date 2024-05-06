417886db366538d70180eb8a48c6ed41bb4b6358
plyshka <leruop@gmail.com>
2024-05-07 02:08:22 +0500

    Re-add CommandExt for windows

--- alvr/dashboard/src/steamvr_launcher/windows_steamvr.rs
+++ alvr/dashboard/src/steamvr_launcher/windows_steamvr.rs
@@ -4,6 +4,7 @@ pub mod windows_steamvr {
     const CREATE_NO_WINDOW: u32 = 0x0800_0000;
 
     pub fn start_steamvr() {
+        use std::os::windows::process::CommandExt;
         Command::new("cmd")
             .args(["/C", "start", "steam://rungameid/250820"])
             .creation_flags(CREATE_NO_WINDOW)
@@ -12,6 +13,7 @@ pub mod windows_steamvr {
     }
 
     pub fn kill_process(pid: u32) {
+        use std::os::windows::process::CommandExt;
         Command::new("taskkill.exe")
             .args(["/PID", &pid.to_string(), "/F"])
             .creation_flags(CREATE_NO_WINDOW)

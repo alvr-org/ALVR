6eec7f9a46fb73f92b064c15155d520efc41b6cc
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-06-02 17:51:36 +0200

    client gfx refactor (3): init destroy c++

--- alvr/client_core/cpp/bindings.h
+++ alvr/client_core/cpp/bindings.h
@@ -35,13 +35,13 @@ extern "C" unsigned int LOBBY_ROOM_BIN_LEN;
 
 // graphics.h
 extern "C" void initGraphicsNative();
-extern "C" void destroyGraphicsNative();
 extern "C" void prepareLobbyRoom(int viewWidth,
                                  int viewHeight,
                                  const unsigned int *swapchainTextures[2],
                                  int swapchainLength,
                                  bool enable_srgb_correction);
-extern "C" void destroyRenderers();
+extern "C" void destroyLobby();
+extern "C" void destroyStream();
 extern "C" void streamStartNative(FfiStreamConfig config);
 extern "C" void updateLobbyHudTexture(const unsigned char *data);
 extern "C" void renderLobbyNative(const FfiViewInput eyeInputs[2]);

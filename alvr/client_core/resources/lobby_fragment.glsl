d6920f1c18205c15af430d0954ad476f6c9a333d
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-07-01 15:46:16 +0200

    client gfx refactor (7): partially port lobby

---
+++ alvr/client_core/resources/lobby_fragment.glsl
@@ -0,0 +1,77 @@
+#version 300 es
+
+uniform lowp int object_type;
+uniform sampler2D hud_texture;
+
+in vec2 uv;
+in vec3 position;
+
+out vec4 out_color;
+
+void main() {
+    if(object_type == 0) { // Ground
+        lowp vec3 groundCenter = vec3(0.0f, 0.0f, 0.0f);
+        lowp vec3 groundHorizon = vec3(0.0f, 0.0f, 0.015f);
+
+        lowp vec3 gridClose = vec3(0.114f, 0.545f, 0.804f);
+        lowp vec3 gridFar = vec3(0.259f, 0.863f, 0.886f);
+
+        lowp float lineFadeStart = 10.0f;
+        lowp float lineFadeEnd = 50.0f;
+        lowp float lineFadeDist = lineFadeEnd - lineFadeStart;
+
+        lowp float lineBloom = 10.0f;
+
+        lowp float distance = length(position.xz);
+
+        // Pick a coordinate to visualize in a grid
+        lowp vec2 coord = position.xz / 2.0f;
+
+        // Compute anti-aliased world-space grid lines
+        lowp vec2 grid = abs(fract(coord - 0.5f) - 0.5f) / fwidth(coord);
+
+        // Create mask for grid lines and fade over distance
+        lowp float line = clamp(1.0f - min(grid.x, grid.y), 0.0f, 1.0f);
+        line *= clamp((lineFadeStart - distance) / lineFadeDist, 0.0f, 1.0f);
+
+        // Fill in normal ground colour
+        out_color.rgb = groundCenter * (1.0f - line);
+
+        // Add cheap and simple "bloom" to the grid lines
+        line *= 1.0f + lineBloom;
+
+        // Fill in grid line colour
+        out_color.rgb += line * mix(gridFar, gridClose, clamp((lineFadeEnd - distance) / lineFadeEnd, 0.0f, 1.0f));
+
+        // Fade to the horizon colour over distance
+        if(distance > 10.0f) {
+            lowp float coef = 1.0f - 10.0f / distance;
+            out_color.rgb = (1.0f - coef) * out_color.rgb + coef * groundHorizon;
+        }
+
+        out_color.a = 1.0f;
+    } else if(object_type == 1) { // HUD
+        lowp vec3 textColor = vec3(1.0f, 1.0f, 1.0f);
+
+        out_color.rgb = textColor;
+        out_color.a = texture(hud_texture, uv).a;
+    } else if(object_type == 2) { // Sky
+        lowp vec3 skyUpColor = vec3(0.0f, 0.0f, 0.0f);
+        lowp vec3 skyHorizonColor = vec3(0.0f, 0.0f, 0.02f);
+
+        lowp float coef = 1.0f;
+        if(position.y < 50.0f) {
+            coef = position.y / 100.0f;
+        } else if(position.y < 100.0f) {
+            coef = (position.y - 50.0f) / 50.0f * 0.3f + 0.5f;
+        } else {
+            coef = (position.y - 100.0f) / 150.0f * 0.2f + 0.8f;
+        }
+        out_color.a = 1.0f;
+        out_color.rgb = skyUpColor * coef + skyHorizonColor * (1.0f - coef);
+
+        // out_color.a = texture(hud_texture, uv).a;
+    } else if(object_type == 3) { // Hands
+        out_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+}
--- alvr/client_core/src/graphics/lobby.rs
+++ alvr/client_core/src/graphics/lobby.rs
@@ -1,46 +1,111 @@
-use super::{GraphicsContext, RenderViewInput};
-use alvr_common::glam::UVec2;
+use super::{ck, GraphicsContext, RenderTarget, RenderViewInput};
+use alvr_common::{
+    glam::{IVec2, Mat4, UVec2, Vec3, Vec4},
+    Fov,
+};
+use glow::{self as gl, HasContext, PixelUnpackData};
 use glyph_brush_layout::{
     ab_glyph::{Font, FontRef, ScaleFont},
     FontId, GlyphPositioner, HorizontalAlign, Layout, SectionGeometry, SectionText, VerticalAlign,
 };
-use std::rc::Rc;
+use std::{f32::consts::FRAC_PI_2, num::NonZeroU32, rc::Rc};
 
-const HUD_TEXTURE_WIDTH: usize = 1280;
-const HUD_TEXTURE_HEIGHT: usize = 720;
-const FONT_SIZE: f32 = 50_f32;
+const HUD_DIST: f32 = 5.0;
+const HUD_SIDE: f32 = 3.5;
+const HUD_TEXTURE_SIDE: usize = 2048;
+const FONT_SIZE: f32 = 100.0;
+
+fn projection_from_fov(fov: Fov) -> Mat4 {
+    const NEAR: f32 = 0.1;
+
+    let tanl = f32::tan(fov.left);
+    let tanr = f32::tan(fov.right);
+    let tanu = f32::tan(fov.up);
+    let tand = f32::tan(fov.down);
+    let a = 2.0 / (tanr - tanl);
+    let b = 2.0 / (tanu - tand);
+    let c = (tanr + tanl) / (tanr - tanl);
+    let d = (tanu + tand) / (tanu - tand);
+
+    Mat4::from_cols(
+        Vec4::new(a, 0.0, c, 0.0),
+        Vec4::new(0.0, b, d, 0.0),
+        Vec4::new(0.0, 0.0, -1.0, -2.0 * NEAR),
+        Vec4::new(0.0, 0.0, -1.0, 0.0),
+    )
+    .transpose()
+}
 
 pub struct LobbyRenderer {
-    _context: Rc<GraphicsContext>,
+    context: Rc<GraphicsContext>,
+    program: gl::Program,
+    object_type_uloc: gl::UniformLocation,
+    transform_uloc: gl::UniformLocation,
+    squeeze_amount_uloc: gl::UniformLocation,
+    hud_texture: gl::Texture,
+    targets: [Vec<RenderTarget>; 2],
+    viewport_size: IVec2,
 }
 
 impl LobbyRenderer {
-    #[allow(unused_variables)]
     pub fn new(
         context: Rc<GraphicsContext>,
-        preferred_view_resolution: UVec2,
+        view_resolution: UVec2,
         swapchain_textures: [Vec<u32>; 2],
-        enable_srgb_correction: bool,
         initial_hud_message: &str,
     ) -> Self {
-        #[cfg(target_os = "android")]
-        unsafe {
-            let swapchain_length = swapchain_textures[0].len();
-            let mut swapchain_textures = [
-                swapchain_textures[0].as_ptr(),
-                swapchain_textures[1].as_ptr(),
-            ];
+        let gl = &context.gl_context;
 
-            super::opengl::prepareLobbyRoom(
-                preferred_view_resolution.x as _,
-                preferred_view_resolution.y as _,
-                swapchain_textures.as_mut_ptr(),
-                swapchain_length as _,
-                enable_srgb_correction,
-            );
-        }
+        let render_targets = [
+            swapchain_textures[0]
+                .iter()
+                .map(|tex| {
+                    RenderTarget::new(
+                        Rc::clone(&context),
+                        gl::NativeTexture(NonZeroU32::new(*tex).unwrap()),
+                    )
+                })
+                .collect(),
+            swapchain_textures[1]
+                .iter()
+                .map(|tex| {
+                    RenderTarget::new(
+                        Rc::clone(&context),
+                        gl::NativeTexture(NonZeroU32::new(*tex).unwrap()),
+                    )
+                })
+                .collect(),
+        ];
 
-        let this = Self { _context: context };
+        let hud_texture = super::create_texture(
+            gl,
+            UVec2::new(HUD_TEXTURE_SIDE as u32, HUD_TEXTURE_SIDE as u32),
+            gl::RGBA8,
+        );
+
+        let program = super::create_program(
+            gl,
+            include_str!("../../resources/lobby_vertex.glsl"),
+            include_str!("../../resources/lobby_fragment.glsl"),
+        );
+
+        let this = unsafe {
+            let object_type_uloc = ck!(gl.get_uniform_location(program, "object_type").unwrap());
+            let transform_uloc = ck!(gl.get_uniform_location(program, "transform").unwrap());
+            let squeeze_amount_uloc =
+                ck!(gl.get_uniform_location(program, "squeeze_amount").unwrap());
+
+            Self {
+                context,
+                program,
+                object_type_uloc,
+                transform_uloc,
+                squeeze_amount_uloc,
+                hud_texture,
+                targets: render_targets,
+                viewport_size: view_resolution.as_ivec2(),
+            }
+        };
 
         this.update_hud_message(initial_hud_message);
 
@@ -58,8 +123,8 @@ impl LobbyRenderer {
                 &[&ubuntu_font],
                 &SectionGeometry {
                     screen_position: (
-                        HUD_TEXTURE_WIDTH as f32 / 2_f32,
-                        HUD_TEXTURE_HEIGHT as f32 / 2_f32,
+                        HUD_TEXTURE_SIDE as f32 / 2_f32,
+                        HUD_TEXTURE_SIDE as f32 / 2_f32,
                     ),
                     ..Default::default()
                 },
@@ -72,7 +137,7 @@ impl LobbyRenderer {
 
         let scaled_font = ubuntu_font.as_scaled(FONT_SIZE);
 
-        let mut buffer = vec![0_u8; HUD_TEXTURE_WIDTH * HUD_TEXTURE_HEIGHT * 4];
+        let mut buffer = vec![0; HUD_TEXTURE_SIDE * HUD_TEXTURE_SIDE * 4];
 
         for section_glyph in section_glyphs {
             if let Some(outlined) = scaled_font.outline_glyph(section_glyph.glyph) {
@@ -80,52 +145,107 @@ impl LobbyRenderer {
                 outlined.draw(|x, y, alpha| {
                     let x = x as usize + bounds.min.x as usize;
                     let y = y as usize + bounds.min.y as usize;
-                    buffer[(y * HUD_TEXTURE_WIDTH + x) * 4 + 3] = (alpha * 255.0) as u8;
+                    if x < HUD_TEXTURE_SIDE && y < HUD_TEXTURE_SIDE {
+                        buffer[(y * HUD_TEXTURE_SIDE + x) * 4 + 3] = (alpha * 255.0) as u8;
+                    }
                 });
             }
         }
 
-        #[cfg(target_os = "android")]
+        let gl = &self.context.gl_context;
         unsafe {
-            super::opengl::updateLobbyHudTexture(buffer.as_ptr());
+            ck!(gl.bind_texture(gl::TEXTURE_2D, Some(self.hud_texture)));
+            ck!(gl.tex_sub_image_2d(
+                gl::TEXTURE_2D,
+                0,
+                0,
+                0,
+                HUD_TEXTURE_SIDE as i32,
+                HUD_TEXTURE_SIDE as i32,
+                gl::RGBA,
+                gl::UNSIGNED_BYTE,
+                PixelUnpackData::Slice(&buffer),
+            ));
         }
     }
 
     #[allow(unused_variables)]
     pub fn render(&self, view_inputs: [RenderViewInput; 2]) {
-        #[cfg(target_os = "android")]
-        unsafe {
-            let eye_inputs = [
-                super::opengl::FfiViewInput {
-                    position: view_inputs[0].pose.position.to_array(),
-                    orientation: view_inputs[0].pose.orientation.to_array(),
-                    fovLeft: view_inputs[0].fov.left,
-                    fovRight: view_inputs[0].fov.right,
-                    fovUp: view_inputs[0].fov.up,
-                    fovDown: view_inputs[0].fov.down,
-                    swapchainIndex: view_inputs[0].swapchain_index as _,
-                },
-                super::opengl::FfiViewInput {
-                    position: view_inputs[1].pose.position.to_array(),
-                    orientation: view_inputs[1].pose.orientation.to_array(),
-                    fovLeft: view_inputs[1].fov.left,
-                    fovRight: view_inputs[1].fov.right,
-                    fovUp: view_inputs[1].fov.up,
-                    fovDown: view_inputs[1].fov.down,
-                    swapchainIndex: view_inputs[1].swapchain_index as _,
-                },
-            ];
+        let gl = &self.context.gl_context;
 
-            super::opengl::renderLobbyNative(eye_inputs.as_ptr());
+        for (view_idx, view_input) in view_inputs.iter().enumerate() {
+            self.targets[view_idx][view_input.swapchain_index as usize].bind();
+
+            let view = Mat4::from_rotation_translation(
+                view_input.pose.orientation,
+                view_input.pose.position,
+            )
+            .inverse();
+            let view_proj = projection_from_fov(view_input.fov) * view;
+
+            unsafe {
+                ck!(gl.use_program(Some(self.program)));
+
+                ck!(gl.disable(gl::SCISSOR_TEST));
+                ck!(gl.disable(gl::DEPTH_TEST));
+                ck!(gl.disable(gl::CULL_FACE));
+
+                // gl.clear_color(0.88, 0.95, 0.95, 1.0);
+                ck!(gl.clear_color(0.0, 0.0, 0.02, 1.0));
+                ck!(gl.clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT));
+
+                ck!(gl.enable(gl::BLEND));
+                ck!(gl.blend_func(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA));
+
+                ck!(gl.viewport(0, 0, self.viewport_size.x, self.viewport_size.y));
+
+                // Draw the following geometry in the correct order (depth buffer is disabled)
+
+                // // Render sky
+                // gl.uniform_1_i32(Some(&self.object_type_uloc), 2);
+                // gl.draw_arrays(gl::TRIANGLE_STRIP, 0, 4);
+
+                // Render ground
+                ck!(gl.uniform_1_i32(Some(&self.object_type_uloc), 0));
+                ck!(gl.uniform_matrix_4_f32_slice(
+                    Some(&self.transform_uloc),
+                    false,
+                    &view_proj.to_cols_array(),
+                ));
+                ck!(gl.draw_arrays(gl::TRIANGLE_STRIP, 0, 4));
+
+                // Render HUD
+                // todo: draw only one HUD panel and implement lazy follow
+                ck!(gl.uniform_1_i32(Some(&self.object_type_uloc), 1));
+                ck!(gl.active_texture(gl::TEXTURE0));
+                ck!(gl.bind_texture(gl::TEXTURE_2D, Some(self.hud_texture)));
+                for i in 0..4 {
+                    let panel_pos = Mat4::from_rotation_y(FRAC_PI_2 * i as f32)
+                        * Mat4::from_translation(Vec3::new(0.0, HUD_SIDE / 2.0, -HUD_DIST))
+                        * Mat4::from_scale(Vec3::ONE * HUD_SIDE);
+                    ck!(gl.uniform_matrix_4_f32_slice(
+                        Some(&self.transform_uloc),
+                        false,
+                        &(view_proj * panel_pos).to_cols_array(),
+                    ));
+                    ck!(gl.draw_arrays(gl::TRIANGLE_STRIP, 0, 4));
+                }
+
+                // // Render hands
+                // gl.uniform_1_i32(Some(&self.object_type_uloc), 3);
+                // gl.draw_arrays(gl::LINES, 0, 16);
+            }
         }
     }
 }
 
 impl Drop for LobbyRenderer {
     fn drop(&mut self) {
-        #[cfg(target_os = "android")]
+        let gl = &self.context.gl_context;
+
         unsafe {
-            super::opengl::destroyLobby();
+            ck!(gl.delete_texture(self.hud_texture));
+            ck!(gl.delete_program(self.program));
         }
     }
 }

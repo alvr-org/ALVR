41355323e49b4a1dc5b3eeae37397dbb3169a0b2
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-07-02 02:35:57 +0200

    feat(client): :sparkles: Show hands in lobby

--- alvr/client_core/resources/lobby_vertex.glsl
+++ alvr/client_core/resources/lobby_vertex.glsl
@@ -22,15 +22,7 @@ void main() {
     } else if(object_type == 1) { // HUD
         gl_Position = transform * vec4(norm_vert_a - 0.5f, norm_vert_b - 0.5f, 0.0f, 1.0f);
         uv = vec2(norm_vert_a, 1.0f - norm_vert_b);
-    } else if(object_type == 2) { // Sky
-        // make headlocked quad, then inverse transform it for world position
-        gl_Position = vec4((norm_vert_a - 0.5f) * SKY_SIDE, (norm_vert_b - 0.5f) * SKY_SIDE, FAR, 1.0f);
-        position = (transform * gl_Position).xyz;
-    } else if(object_type == 3) { // Hands
-        vec2 sides_dir = vec2(cos(norm_vert_b * PI / 2.0f), sin(norm_vert_b * PI / 2.0f));
-        float squeeze_multiplier = mix(1.0f, HAND_SQUEEZE_MULTIPLIER, squeeze_amount);
-        float xz_size = HAND_BASE_HALF_SIDE / squeeze_multiplier;
-        float y_size = HAND_BASE_HALF_SIDE * squeeze_multiplier;
-        gl_Position = transform * vec4(sides_dir.x * xz_size, y_size, sides_dir.y * xz_size, 1.0f);
+    } else if(object_type == 2) { // Hands (single bone)
+        gl_Position = transform * vec4(0.0f, 0.0f, -float(gl_VertexID), 1.0f);
     }
 }

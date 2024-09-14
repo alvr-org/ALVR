3436a53ca8f3ac2f8843034404a6fa940052e3df
zarik5 <riccardo.zaglia5@gmail.com>
2024-09-14 23:37:39 +0200

    fix(client_core): :bug: Workaround for blank right eye view (#2397)

--- alvr/client_core/resources/staging_vertex.glsl
+++ alvr/client_core/resources/staging_vertex.glsl
@@ -1,8 +1,11 @@
 #version 300 es
 
+uniform int view_idx;
+
 out vec2 uv;
 
 void main() {
-    uv = vec2(gl_VertexID & 1, gl_VertexID >> 1);
-    gl_Position = vec4((uv - 0.5f) * 2.f, 0, 1);
+    vec2 screen_uv = vec2(gl_VertexID & 1, gl_VertexID >> 1);
+    gl_Position = vec4((screen_uv - 0.5f) * 2.f, 0, 1);
+    uv = vec2((screen_uv.x + float(view_idx)) / 2.f, screen_uv.y);
 }

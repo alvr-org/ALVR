e804658be02b32fb2febae02326084dcd749632f
E1int <110526002+E1int@users.noreply.github.com>
2024-10-13 20:49:25 +0200

    Ignore first line of device list

--- alvr/adb/src/lib.rs
+++ alvr/adb/src/lib.rs
@@ -65,7 +65,7 @@ pub fn install_apk(adb_path: &str, device_serial: &str, apk_path: &str) -> anyho
 pub fn list_devices(adb_path: &str) -> anyhow::Result<Vec<Device>> {
     let output = Command::new(adb_path).args(["devices", "-l"]).output()?;
     let text = String::from_utf8_lossy(&output.stdout);
-    let devices = text.lines().filter_map(device::parse).collect();
+    let devices = text.lines().skip(1).filter_map(device::parse).collect();
     Ok(devices)
 }
 

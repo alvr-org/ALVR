2c0e3ef35a071de3fce02ce60fbeb811b203cd9e
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-10-14 02:36:42 +0200

    refactor(server_openvr): :recycle: Refactor around TrackedDevice

--- alvr/server_openvr/cpp/alvr_server/HMD.h
+++ alvr/server_openvr/cpp/alvr_server/HMD.h
@@ -18,55 +18,25 @@ class CD3DRender;
 #endif
 class PoseHistory;
 
-class Hmd : public TrackedDevice, public vr::ITrackedDeviceServerDriver, vr::IVRDisplayComponent {
+class Hmd : public TrackedDevice, vr::IVRDisplayComponent {
 public:
+    std::shared_ptr<PoseHistory> m_poseHistory;
+    std::shared_ptr<CEncoder> m_encoder;
+
     Hmd();
-
     virtual ~Hmd();
-
-    virtual vr::EVRInitError Activate(vr::TrackedDeviceIndex_t unObjectId);
-    virtual void Deactivate();
-    virtual void EnterStandby() { }
-    void* GetComponent(const char* pchComponentNameAndVersion);
-    virtual void DebugRequest(const char*, char*, uint32_t) { }
-    virtual vr::DriverPose_t GetPose();
-
     void OnPoseUpdated(uint64_t targetTimestampNs, FfiDeviceMotion motion);
-
     void StartStreaming();
-
     void StopStreaming();
-
     void SetViewsConfig(FfiViewsConfig config);
 
-    vr::ETrackedDeviceClass GetDeviceClass() const { return m_deviceClass; }
-    bool IsTrackingRef() const { return m_deviceClass == vr::TrackedDeviceClass_TrackingReference; }
-    bool IsHMD() const { return m_deviceClass == vr::TrackedDeviceClass_HMD; }
-
-    // IVRDisplayComponent
-
-    virtual void GetWindowBounds(int32_t* x, int32_t* y, uint32_t* width, uint32_t* height);
-    virtual bool IsDisplayOnDesktop() { return false; }
-    virtual bool IsDisplayRealDisplay();
-    virtual void GetRecommendedRenderTargetSize(uint32_t* width, uint32_t* height);
-    virtual void GetEyeOutputViewport(
-        vr::EVREye eye, uint32_t* x, uint32_t* y, uint32_t* width, uint32_t* height
-    );
-    virtual void
-    GetProjectionRaw(vr::EVREye eEye, float* pfLeft, float* pfRight, float* pfTop, float* pfBottom);
-    virtual vr::DistortionCoordinates_t ComputeDistortion(vr::EVREye eEye, float fU, float fV);
-
+private:
     vr::VRInputComponentHandle_t m_proximity;
 
-    std::shared_ptr<CEncoder> m_encoder;
-    std::shared_ptr<PoseHistory> m_poseHistory;
-
-private:
     FfiViewsConfig views_config;
 
     bool m_baseComponentsInitialized;
     bool m_streamComponentsInitialized;
-    vr::ETrackedDeviceClass m_deviceClass;
 
     vr::HmdMatrix34_t m_eyeToHeadLeft;
     vr::HmdMatrix34_t m_eyeToHeadRight;
@@ -85,9 +55,23 @@ private:
 
     std::shared_ptr<ViveTrackerProxy> m_viveTrackerProxy;
 
-    vr::DriverPose_t m_pose = {};
-
 #ifndef _WIN32
     bool m_refreshRateSet = false;
 #endif
+
+    // TrackedDevice
+    virtual bool activate() final;
+    virtual void* get_component(const char* component_name_and_version) final;
+
+    // IVRDisplayComponent
+    virtual void GetWindowBounds(int32_t* x, int32_t* y, uint32_t* width, uint32_t* height);
+    virtual bool IsDisplayOnDesktop() { return false; }
+    virtual bool IsDisplayRealDisplay();
+    virtual void GetRecommendedRenderTargetSize(uint32_t* width, uint32_t* height);
+    virtual void GetEyeOutputViewport(
+        vr::EVREye eye, uint32_t* x, uint32_t* y, uint32_t* width, uint32_t* height
+    );
+    virtual void
+    GetProjectionRaw(vr::EVREye eEye, float* pfLeft, float* pfRight, float* pfTop, float* pfBottom);
+    virtual vr::DistortionCoordinates_t ComputeDistortion(vr::EVREye eEye, float fU, float fV);
 };

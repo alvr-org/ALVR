236a75441573d73ef6344702598e0ed4cf2cbf26
Grillo del Mal <grillo@delmal.cl>
2024-10-25 02:44:54 -0300

    VMC: Weird constant collision error

--- alvr/server_core/src/tracking/vmc.rs
+++ alvr/server_core/src/tracking/vmc.rs
@@ -56,7 +56,7 @@ static DEVICE_MOTIONS_ROTATION_MAP: Lazy<HashMap<u64, Quat>> = Lazy::new(|| {
         ),
         (
             *BODY_LEFT_FOOT_ID,
-            Quat::from_xyzw(-0.59103, 0.38818, 0.00000, -0.70711),
+            Quat::from_xyzw(-0.59103, 0.38818, 0.00000, -0.7071100),
         ),
         (
             *BODY_RIGHT_KNEE_ID,
@@ -64,7 +64,7 @@ static DEVICE_MOTIONS_ROTATION_MAP: Lazy<HashMap<u64, Quat>> = Lazy::new(|| {
         ),
         (
             *BODY_RIGHT_FOOT_ID,
-            Quat::from_xyzw(0.70228, -0.08246, 0.70711, 0.00000),
+            Quat::from_xyzw(0.70228, -0.08246, 0.7071100, 0.00000),
         ),
     ])
 });

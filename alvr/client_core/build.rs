dd4ff21c7317b60aa0c3880851456d6aed3d7248
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2024-11-21 02:19:24 +0100

    refactor(client_core): :recycle: Port FFE to Rust and remove all C++ code

--- alvr/client_core/build.rs
+++ alvr/client_core/build.rs
@@ -1,49 +1,7 @@
-use std::{env, path::PathBuf};
-
 fn main() {
-    let platform_name = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let platform_name = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if platform_name == "android" {
-        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-        let cpp_paths = walkdir::WalkDir::new("cpp")
-            .into_iter()
-            .filter_map(|maybe_entry| maybe_entry.ok())
-            .map(|entry| entry.into_path())
-            .collect::<Vec<_>>();
-
-        if cfg!(feature = "use-cpp") {
-            let source_files_paths = cpp_paths
-                .iter()
-                .filter(|&path| {
-                    path.extension()
-                        .filter(|ext| ext.to_string_lossy() == "cpp")
-                        .is_some()
-                })
-                .cloned()
-                .collect::<Vec<_>>();
-
-            cc::Build::new()
-                .cpp(true)
-                .files(source_files_paths)
-                .include("cpp")
-                .include("cpp/gl_render_utils")
-                .flag("-std=c++17")
-                .flag("-fexceptions")
-                .flag("-frtti")
-                .cpp_link_stdlib("c++_static")
-                .compile("bindings");
-
-            bindgen::builder()
-                .clang_arg("-xc++")
-                .header("cpp/bindings.h")
-                .derive_default(true)
-                .generate()
-                .unwrap()
-                .write_to_file(out_dir.join("bindings.rs"))
-                .unwrap();
-        }
-
         println!("cargo:rustc-link-lib=log");
         println!("cargo:rustc-link-lib=EGL");
         println!("cargo:rustc-link-lib=GLESv3");
@@ -51,9 +9,5 @@ fn main() {
 
         #[cfg(feature = "link-stdcpp-shared")]
         println!("cargo:rustc-link-lib=c++_shared");
-
-        for path in cpp_paths {
-            println!("cargo:rerun-if-changed={}", path.to_string_lossy());
-        }
     }
 }

4e7b74f3a383614ea77fa85b7e587e377184cdd2
zmerp <riccardo.zaglia5@gmail.com>
2024-11-29 16:16:44 +0100

    refactor(client_openxr): :recycle: Do not recreate OpenXR session (#2519)
    * refactor(client_openxr): :recycle: Do not recreate OpenXR session
    
    * Address comments

--- alvr/client_openxr/src/extra_extensions/multimodal_input.rs
+++ alvr/client_openxr/src/extra_extensions/multimodal_input.rs
@@ -14,9 +14,16 @@ pub const META_DETACHED_CONTROLLERS_EXTENSION_NAME: &str = "XR_META_detached_con
 
 static TYPE_SIMULTANEOUS_HANDS_AND_CONTROLLERS_TRACKING_RESUME_INFO_META: Lazy<xr::StructureType> =
     Lazy::new(|| xr::StructureType::from_raw(1000532002));
+static TYPE_SIMULTANEOUS_HANDS_AND_CONTROLLERS_TRACKING_PAUSE_INFO_META: Lazy<xr::StructureType> =
+    Lazy::new(|| xr::StructureType::from_raw(1000532003));
 
 #[repr(C)]
 pub struct SimultaneousHandsAndControllersTrackingResumeInfoMETA {
+    ty: xr::StructureType,
+    next: *const c_void,
+}
+#[repr(C)]
+pub struct SimultaneousHandsAndControllersTrackingPauseInfoMETA {
     ty: xr::StructureType,
     next: *const c_void,
 }
@@ -26,8 +33,13 @@ pub type ResumeSimultaneousHandsAndControllersTrackingMETA =
         sys::Session,
         *const SimultaneousHandsAndControllersTrackingResumeInfoMETA,
     ) -> sys::Result;
+pub type PauseSimultaneousHandsAndControllersTrackingMETA =
+    unsafe extern "system" fn(
+        sys::Session,
+        *const SimultaneousHandsAndControllersTrackingPauseInfoMETA,
+    ) -> sys::Result;
 
-pub fn resume_simultaneous_hands_and_controllers_tracking<G>(
+pub fn resume_simultaneous_hands_and_controllers_tracking_meta<G>(
     session: &xr::Session<G>,
 ) -> xr::Result<()> {
     let resume_simultaneous_hands_and_controllers_tracking_meta = unsafe {
@@ -48,13 +60,39 @@ pub fn resume_simultaneous_hands_and_controllers_tracking<G>(
         ty: *TYPE_SIMULTANEOUS_HANDS_AND_CONTROLLERS_TRACKING_RESUME_INFO_META,
         next: ptr::null(),
     };
-
     unsafe {
         super::xr_res(resume_simultaneous_hands_and_controllers_tracking_meta(
             session.as_raw(),
             &resume_info,
-        ))?;
+        ))
     }
+}
 
-    Ok(())
+pub fn pause_simultaneous_hands_and_controllers_tracking_meta<G>(
+    session: &xr::Session<G>,
+) -> xr::Result<()> {
+    let pause_simultaneous_hands_and_controllers_tracking_meta = unsafe {
+        let mut pause_simultaneous_hands_and_controllers_tracking_meta = None;
+        let _ = (session.instance().fp().get_instance_proc_addr)(
+            session.instance().as_raw(),
+            c"xrPauseSimultaneousHandsAndControllersTrackingMETA".as_ptr(),
+            &mut pause_simultaneous_hands_and_controllers_tracking_meta,
+        );
+
+        pause_simultaneous_hands_and_controllers_tracking_meta.map(|pfn| {
+            mem::transmute::<VoidFunction, PauseSimultaneousHandsAndControllersTrackingMETA>(pfn)
+        })
+    }
+    .ok_or(sys::Result::ERROR_EXTENSION_NOT_PRESENT)?;
+
+    let pause_info = SimultaneousHandsAndControllersTrackingPauseInfoMETA {
+        ty: *TYPE_SIMULTANEOUS_HANDS_AND_CONTROLLERS_TRACKING_PAUSE_INFO_META,
+        next: ptr::null(),
+    };
+    unsafe {
+        super::xr_res(pause_simultaneous_hands_and_controllers_tracking_meta(
+            session.as_raw(),
+            &pause_info,
+        ))
+    }
 }

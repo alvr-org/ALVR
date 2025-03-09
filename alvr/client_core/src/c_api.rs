94d57115f0a06200cb83e04a786c6718e4bad043
Mathew Tkachuk <me@crx.moe>
2025-03-09 19:03:07 +0300

    feat: Pico Motion Trackers object tracking support (#2712)
    * feat: Pico object motion tracking
    
    * style: uses formatting
    
    * refactor: Make draw_crosshair function
    
    * feat: Add velocity for object tracking
    
    * style: Match Bytedance official naming
    
    * refactor: Store Session instead of Instance
    
    * refactor: Implement MotionTrackerBD
    
    * refactor: Split body skeleton data back
    
    * chore: Allow clippy's `too_many_arguments`
    
    * chore: Expect clippy's `too_many_arguments`
    
    * refactor: Body and object tracking mode switch
    
    * fix: Reset MotionTrackingBD to None
    
    * refactor: Flatten BodyTrackingBDConfig
    
    * refactor: Make trackers order predictable
    
    * refactor: Add separate trackers for objects
    
    Also fixes full body tracking if body_tracking_fb is disabled.
    
    * refactor: Revert Add separate trackers for objects
    
    * refactor: Capacity change, fullbody fix, crosshair
    
    * refactor: Correct addressed issues
    
    * refactor: Bring back BodyTrackingBDConfig enum
    
    * chore: Add help strings for body tracking options
    
    * Add help string about tracking mode selection

--- alvr/client_core/src/c_api.rs
+++ alvr/client_core/src/c_api.rs
@@ -840,6 +840,7 @@ pub unsafe extern "C" fn alvr_render_lobby_opengl(
                 [(None, None), (None, None)],
                 None,
                 None,
+                None,
                 render_background,
                 false,
             );

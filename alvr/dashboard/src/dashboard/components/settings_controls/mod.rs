4e81feb667eeb0b2bfcfe1f7e0b926ff077307cb
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2025-03-11 19:02:57 +0100

    refactor(dashboard): :recycle: Rename serde_values_eq -> json_values_eq

--- alvr/dashboard/src/dashboard/components/settings_controls/mod.rs
+++ alvr/dashboard/src/dashboard/components/settings_controls/mod.rs
@@ -57,7 +57,7 @@ pub fn f64_eq(f1: f64, f2: f64) -> bool {
     // format!("{:.6}", f1) == format!("{:.6}", f2)
 }
 
-pub fn serde_values_eq(a: &serde_json::Value, b: &serde_json::Value) -> bool {
+pub fn json_values_eq(a: &serde_json::Value, b: &serde_json::Value) -> bool {
     if let (serde_json::Value::Number(n1), serde_json::Value::Number(n2)) = (a, b) {
         // Note: is_f64() will exclude integers, while just as_f64() will silently do the conversion
         if n1.is_f64() || n2.is_f64() {

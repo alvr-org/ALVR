ba1694ebe84383bd6bb280dd751372d3d204f8fa
Leonhard Saam <54042101+The-personified-devil@users.noreply.github.com>
2025-03-28 16:05:25 +0100

    fix the format thingy

--- alvr/server_openvr/cpp/alvr_server/Controller.h
+++ alvr/server_openvr/cpp/alvr_server/Controller.h
@@ -8,7 +8,7 @@
 class Controller : public TrackedDevice {
 public:
     Controller(uint64_t deviceID, vr::EVRSkeletalTrackingLevel skeletonLevel);
-    virtual ~Controller() { };
+    virtual ~Controller() {};
     void RegisterButton(uint64_t id);
     void SetButton(uint64_t id, FfiButtonValue value);
     bool OnPoseUpdate(uint64_t targetTimestampNs, float predictionS, FfiHandData handData);

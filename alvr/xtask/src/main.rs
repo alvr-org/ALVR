ac236d855e87ddfe9ed7bce60a764117d4332645
zmerp <riccardo.zaglia5@gmail.com>
2025-06-16 00:53:52 +1000

    feat(xtask): :sparkles: Exit with an error code when appropriate (#2823)
    * feat(xtask): :sparkles: Exit with an error code when appropriate
    
    * Address review comments

--- alvr/xtask/src/main.rs
+++ alvr/xtask/src/main.rs
@@ -12,7 +12,7 @@ use alvr_filesystem as afs;
 use dependencies::OpenXRLoadersSelection;
 use packaging::ReleaseFlavor;
 use pico_args::Arguments;
-use std::{fs, time::Instant};
+use std::{fs, process, time::Instant};
 use xshell::{cmd, Shell};
 
 const HELP_STR: &str = r#"
@@ -59,10 +59,10 @@ FLAGS:
     --pico-store        For package-client subcommand, build for Pico Store
 
 ARGS:
-    --platform <NAME>   Name of the platform (operative system name)
+    --platform <NAME>   Can be one of: windows, linux, macos, android. Can be omitted
     --version <VERSION> Specify version to set with the bump-versions subcommand
     --root <PATH>       Installation root. By default no root is set and paths are calculated using
-                        relative paths, which requires conforming to FHS on Linux.
+                        relative paths, which requires conforming to FHS on Linux
 "#;
 
 enum BuildPlatform {
@@ -70,6 +70,12 @@ enum BuildPlatform {
     Linux,
     Macos,
     Android,
+}
+
+pub fn print_help_and_exit(message: &str) -> ! {
+    eprintln!("\n{message}");
+    eprintln!("{HELP_STR}");
+    process::exit(1);
 }
 
 pub fn run_streamer() {
@@ -185,7 +191,7 @@ fn main() {
             "linux" => BuildPlatform::Linux,
             "macos" => BuildPlatform::Macos,
             "android" => BuildPlatform::Android,
-            _ => panic!("Unrecognized platform."),
+            _ => print_help_and_exit("Unrecognized platform"),
         });
 
         let version: Option<String> = args.opt_value_from_str("--version").unwrap();
@@ -265,21 +271,13 @@ fn main() {
                 }
                 "check-msrv" => version::check_msrv(),
                 "kill-oculus" => kill_oculus_processes(),
-                _ => {
-                    println!("\nUnrecognized subcommand.");
-                    println!("{HELP_STR}");
-                    return;
-                }
+                _ => print_help_and_exit("Unrecognized subcommand."),
             }
         } else {
-            println!("\nWrong arguments.");
-            println!("{HELP_STR}");
-            return;
+            print_help_and_exit("Wrong arguments.");
         }
     } else {
-        println!("\nMissing subcommand.");
-        println!("{HELP_STR}");
-        return;
+        print_help_and_exit("Missing subcommand.");
     }
 
     let elapsed_time = begin_time.elapsed();

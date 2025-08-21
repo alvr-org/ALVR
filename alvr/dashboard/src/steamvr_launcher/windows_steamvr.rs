52e423fc44d035461a017f9723a624d1d9c54f5b
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-08-21 19:04:48 +0200

    Add SteamVR fastlaunch command

--- alvr/dashboard/src/steamvr_launcher/windows_steamvr.rs
+++ alvr/dashboard/src/steamvr_launcher/windows_steamvr.rs
@@ -4,11 +4,17 @@ use std::process::Command;
 const CREATE_NO_WINDOW: u32 = 0x0800_0000;
 
 pub fn start_steamvr() {
-    Command::new("cmd")
-        .args(["/C", "start", "steam://rungameid/250820"])
-        .creation_flags(CREATE_NO_WINDOW)
-        .spawn()
-        .ok();
+    if true {
+        Command::new("C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR\\bin\\win64\\vrstartup.exe")
+            .spawn()
+            .ok();
+    } else {
+        Command::new("cmd")
+            .args(["/C", "start", "steam://rungameid/250820"])
+            .creation_flags(CREATE_NO_WINDOW)
+            .spawn()
+            .ok();
+    }
 }
 
 pub fn kill_process(pid: u32) {

a51dcf55f82e12e81ba97d33247e13d1fdb54992
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-08-28 02:54:17 +0200

    Remove redundant clone and extract settings tab frame margin to theme.rs

--- alvr/gui_common/src/theme.rs
+++ alvr/gui_common/src/theme.rs
@@ -9,6 +9,7 @@ pub const SEPARATOR_BG: Color32 = Color32::from_rgb(69, 69, 69);
 pub const FG: Color32 = Color32::from_rgb(250, 250, 250);
 pub const SCROLLBAR_DOT_DIAMETER: f32 = 20.0;
 pub const SWITCH_DOT_DIAMETER: f32 = SCROLLBAR_DOT_DIAMETER;
+pub const SETTINGS_TAB_FRAME_MARGIN: f32 = 12.0;
 
 pub const OK_GREEN: Color32 = Color32::GREEN;
 pub const KO_RED: Color32 = Color32::RED;
